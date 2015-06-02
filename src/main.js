cf82e10604ade7fb00e0996340c7c414fcf6c2cb
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-06-02 16:20:58 -0400

    trying folding

--- src/main.js
+++ src/main.js
@@ -46,9 +46,9 @@ function parseQuery(qstr){
 function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); } 
 
 function jumpToLine(i) { 
-    var t = editor.charCoords({line: i, ch: 0}, "local").top; 
-    var middleHeight = editor.getScrollerElement().offsetHeight / 2; 
-    editor.scrollTo(null, t - middleHeight - 5); 
+    var t = editor.charCoords({line: i-1, ch: 0}, "local").top; 
+    // var middleHeight = editor.getScrollerElement().offsetHeight / 2; 
+    editor.scrollTo(null, t);// - middleHeight - 5); 
 } 
 
 function selectLine( nLine ){
@@ -75,10 +75,68 @@ function selectLines( _string ){
     }
 }
 
+// function foldByLevel(cm, level) {
+//     foldByNodeOrder(cm, 0);
+//     // initialize vars
+//     var cursor = cm.getCursor();
+//     cursor.ch = 0;
+//     cursor.line = 0;
+//     var range = cm.getViewport();
+//     foldByLevelRec(cm, cursor, range, level);
+// };
+
+// function foldByLevelRec(cm, cursor, range, level) {
+//     if (level > 0) {
+//         var searcher = cm.getSearchCursor("<", cursor, false);
+//         while (searcher.findNext() && searcher.pos.from.line < range.to) {
+//             // unfold the tag
+//             cm.foldCode(searcher.pos.from, null, "unfold");
+//             // move the cursor into the tag
+//             cursor = searcher.pos.from;
+//             cursor.ch = searcher.pos.from.ch + 1;
+//             // find the closing tag
+//             var match = CodeMirror.findMatchingTag(cm, cursor, range);
+//             if (match) {
+//                 if (match.close) {
+//                     // create the inner-range and jump the searcher after the ending tag
+//                     var innerrange = { from: range.from, to: range.to };
+//                     innerrange.from = cursor.line + 1;
+//                     innerrange.to = match.close.to.line;
+//                     // the recursive call
+//                     foldByLevelRec(cm, cursor, innerrange, level - 1);
+//                 }
+//                 // move to the next element in the same tag of this function scope
+//                 var nextcursor = { line: cursor.line, to: cursor.ch };
+//                 if (match.close) {
+//                     nextcursor.line = match.close.to.line;
+//                 }
+//                 nextcursor.ch = 0;
+//                 nextcursor.line = nextcursor.line + 1;
+//                 searcher = cm.getSearchCursor("\t", nextcursor, false);
+//             }
+//         }
+//     }
+// }
+
+// function foldByNodeOrder(cm, node) {
+//     // 0 - fold all
+//     unfoldAll(cm);
+//     node++;
+//     for (var l = cm.firstLine() ; l <= cm.lastLine() ; ++l)
+//         if (node == 0)
+//             cm.foldCode({ line: l, ch: 0 }, null, "fold");
+//         else node--;
+// }
+
+// function unfoldAll() {
+//     for (var i = 0; i < editor.lineCount() ; i++) {
+//         editor.foldCode({ line: i }, null, "unfold");
+//     }
+// }
+
 // CODE EDITOR
 //----------------------------------------------
 function newContent(){
-    console.log("New Content");
     window.location.href = ".";
 }
 
@@ -107,7 +165,6 @@ function saveContent(){
 
 function initEditor(){
     querry = parseQuery(window.location.search.slice(1));
-    console.log(querry);
 
     if (querry['location']){
         style_file = querry['location'];
@@ -125,7 +182,11 @@ function initEditor(){
             mode: "text/x-yaml",
             keyMap: "sublime",
             autoCloseBrackets: true,
-            extraKeys: {"Ctrl-Space": "autocomplete"},
+            extraKeys: { },
+            foldGutter: { 
+                rangeFinder: CodeMirror.fold.indent
+            },
+            gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
             showCursorWhenSelecting: true,
             theme: "tangram",
             lineWrapping: true,
@@ -197,12 +258,7 @@ function initMap() {
 // Events
 //----------------------------------------------
 function resizeMap() {
-    var menu = document.getElementById("menu");
-    var elements = document.getElementsByClassName("CodeMirror-scroll");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].style.height = (window.innerHeight) + 'px';
-        console.log("resize " + i + " to " + elements[i].style.height)
-    }
+    editor.setSize('100%',(window.innerHeight-31) + 'px');
 }
 
 window.addEventListener('resize', resizeMap);
