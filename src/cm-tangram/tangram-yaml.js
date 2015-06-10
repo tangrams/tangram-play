8e0764503fdc1d625867b866efe6025213897f9c
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-06-10 15:27:42 -0400

    mixed commenting working

--- src/cm-tangram/tangram-yaml.js
+++ src/cm-tangram/tangram-yaml.js
@@ -15,6 +15,43 @@ function tags2Address(tags){
     }
 }
 
+// Add Address to token states
+function yamlAddressing(stream, state) {
+    // if (!state.tags) state.tags = [];
+    // if(state.tagLevel !== undefined) state.tagLevel = -1;
+
+    // Once per line compute the TAGS tree, NAME, ADDRESS and LEVEL.
+    if (stream.pos === 0) {
+        var regex = /(^\s*)(\w+):/gm;
+        var tag = regex.exec(stream.string);
+        if ( tag ){
+
+            //  If looks like a tag
+            //  Calculate the number of spaces and indentation level
+            var spaces = (tag[1].match(/\s/g) || []).length
+            var level = Math.floor( spaces  / stream.tabSize );
+
+            //  Update the TAGS tree
+            if ( level > state.tagLevel ) {
+                state.tags.push(tag[2]);
+            } else if (level === state.tagLevel ) {
+                state.tags[level] = tag[2];
+            } if ( level < state.tagLevel ) {
+                var diff = state.tagLevel - level; 
+                for (var i = 0; i < diff; i++){
+                    state.tags.pop();
+                }
+                state.tags[level] = tag[2];
+            }
+
+            //  Record all that in the state value
+            state.tagName = tag[2];
+            state.tagLevel = level;
+            state.tagAddress = tags2Address(state.tags);
+        }
+    }
+}
+
 // Get array of YAML tags parent tree of a particular line
 function getTags(cm, nLine) { return cm.lineInfo(nLine).handle.stateAfter.yamlState.tags; }
 // Get string of YAML tags in a folder style
@@ -59,230 +96,162 @@ function isShader(address){
     return (isGlobalBlock(address) || isPositionBlock(address) || isNormalBlock(address) || isColorBlock(address) || isFilterBlock(address));
 }
 
-// //  YAML (width tag address)
-// //  ===============================================================================
-// (function(mod) {
-//     if (typeof exports == "object" && typeof module == "object") // CommonJS
-//         mod(require("../../lib/codemirror"));
-//     else if (typeof define == "function" && define.amd) // AMD
-//         define(["../../lib/codemirror"], mod);
-//     else // Plain browser env
-//         mod(CodeMirror);
-// })(function(CodeMirror) {
-//     "use strict";
+//  YAML
+//  ===============================================================================
+(function(mod) {
+    if (typeof exports == "object" && typeof module == "object") // CommonJS
+        mod(require("../../lib/codemirror"));
+    else if (typeof define == "function" && define.amd) // AMD
+        define(["../../lib/codemirror"], mod);
+    else // Plain browser env
+        mod(CodeMirror);
+})(function(CodeMirror) {
+    "use strict";
 
-//     CodeMirror.defineMode("yaml", function() {
-//         var cons = ['true', 'false', 'on', 'off', 'yes', 'no'];
-//         var keywordRegex = new RegExp("\\b(("+cons.join(")|(")+"))$", 'i');
+    CodeMirror.defineMode("yaml", function() {
+        var cons = ['true', 'false', 'on', 'off', 'yes', 'no'];
+        var keywordRegex = new RegExp("\\b(("+cons.join(")|(")+"))$", 'i');
 
-//         return {
-//             token: function(stream, state) {
+        return {
+            token: function(stream, state) {
 
-//                 // Once per line compute the TAGS tree, NAME, ADDRESS and LEVEL.
-//                 if (stream.pos == 0) {
-//                     var regex = /(^\s*)(\w+):/gm;
-//                     var tag = regex.exec(stream.string);
-//                     if ( tag ){
+                var ch = stream.peek();
+                var esc = state.escaped;
+                state.escaped = false;
 
-//                         //  If looks like a tag
-//                         //  Calculate the number of spaces and indentation level
-//                         var spaces = (tag[1].match(/\s/g) || []).length
-//                         var level = Math.floor( spaces  / stream.tabSize );
+                /* comments */
+                if (ch == "#" && (stream.pos == 0 || /\s/.test(stream.string.charAt(stream.pos - 1)))) {
+                    stream.skipToEnd();
+                    return "comment";
+                }
 
-//                         //  Update the TAGS tree
-//                         if ( level > state.tagLevel ) {
-//                             state.tags.push(tag[2]);
-//                         } else if (level === state.tagLevel ) {
-//                             state.tags[level] = tag[2];
-//                         } if ( level < state.tagLevel ) {
-//                             var diff = state.tagLevel - level; 
-//                             for (var i = 0; i < diff; i++){
-//                                 state.tags.pop();
-//                             }
-//                             state.tags[level] = tag[2];
-//                         }
+                if (stream.match(/^('([^']|\\.)*'?|"([^"]|\\.)*"?)/)){
+                    return "string";
+                }
 
-//                         //  Record all that in the state value
-//                         state.tagName = tag[2];
-//                         state.tagLevel = level;
-//                         state.tagAddress = tags2Address(state.tags);
-//                     }
-//                 }
+                if (state.literal && stream.indentation() > state.keyCol) {
+                    stream.skipToEnd(); 
+                    return "string";
+                } else if (state.literal) { 
+                    state.literal = false; 
+                }
 
-//                 var ch = stream.peek();
-//                 var esc = state.escaped;
-//                 state.escaped = false;
-
-//                 /* comments */
-//                 if (ch == "#" && (stream.pos == 0 || /\s/.test(stream.string.charAt(stream.pos - 1)))) {
-//                     stream.skipToEnd();
-//                     return "comment";
-//                 }
-
-//                 if (stream.match(/^('([^']|\\.)*'?|"([^"]|\\.)*"?)/)){
-//                     return "string";
-//                 }
-
-//                 if (state.literal && stream.indentation() > state.keyCol) {
-//                     stream.skipToEnd(); 
-//                     return "string";
-//                 } else if (state.literal) { 
-//                     state.literal = false; 
-//                 }
-
-//                 if (stream.sol()) {
-//                     state.keyCol = 0;
-//                     state.pair = false;
-//                     state.pairStart = false;
+                if (stream.sol()) {
+                    state.keyCol = 0;
+                    state.pair = false;
+                    state.pairStart = false;
                 
-//                     /* document start */
-//                     if(stream.match(/---/)) { 
-//                         return "def";
-//                     }
+                    /* document start */
+                    if(stream.match(/---/)) { 
+                        return "def";
+                    }
 
-//                     /* document end */
-//                     if (stream.match(/\.\.\./)) { 
-//                         return "def"; 
-//                     }
+                    /* document end */
+                    if (stream.match(/\.\.\./)) { 
+                        return "def"; 
+                    }
 
-//                     /* array list item */
-//                     if (stream.match(/\s*-\s+/)) { 
-//                         return 'meta'; 
-//                     }
-//                 }
+                    /* array list item */
+                    if (stream.match(/\s*-\s+/)) { 
+                        return 'meta'; 
+                    }
+                }
 
-//                 /* inline pairs/lists */
-//                 if (stream.match(/^(\{|\}|\[|\])/)) {
-//                     if (ch == '{')
-//                         state.inlinePairs++;
-//                     else if (ch == '}')
-//                         state.inlinePairs--;
-//                     else if (ch == '[')
-//                         state.inlineList++;
-//                     else
-//                         state.inlineList--;
-//                     return 'meta';
-//                 }
+                /* inline pairs/lists */
+                if (stream.match(/^(\{|\}|\[|\])/)) {
+                    if (ch == '{')
+                        state.inlinePairs++;
+                    else if (ch == '}')
+                        state.inlinePairs--;
+                    else if (ch == '[')
+                        state.inlineList++;
+                    else
+                        state.inlineList--;
+                    return 'meta';
+                }
 
-//                 /* list seperator */
-//                 if (state.inlineList > 0 && !esc && ch == ',') {
-//                     stream.next();
-//                     return 'meta';
-//                 }
+                /* list seperator */
+                if (state.inlineList > 0 && !esc && ch == ',') {
+                    stream.next();
+                    return 'meta';
+                }
 
-//                 /* pairs seperator */
-//                 if (state.inlinePairs > 0 && !esc && ch == ',') {
-//                     state.keyCol = 0;
-//                     state.pair = false;
-//                     state.pairStart = false;
-//                     stream.next();
-//                     return 'meta';
-//                 }
+                /* pairs seperator */
+                if (state.inlinePairs > 0 && !esc && ch == ',') {
+                    state.keyCol = 0;
+                    state.pair = false;
+                    state.pairStart = false;
+                    stream.next();
+                    return 'meta';
+                }
 
-//                 /* start of value of a pair */
-//                 if (state.pairStart) {
-//                     /* block literals */
-//                     if (stream.match(/^\s*(\||\>)\s*/)) { 
-//                         state.literal = true; 
-//                         return 'meta';
-//                     };
+                /* start of value of a pair */
+                if (state.pairStart) {
+                    /* block literals */
+                    if (stream.match(/^\s*(\||\>)\s*/)) { 
+                        state.literal = true; 
+                        return 'meta';
+                    };
 
-//                     /* references */
-//                     if (stream.match(/^\s*(\&|\*)[a-z0-9\._-]+\b/i)) { 
-//                         return 'variable-2'; 
-//                     }
+                    /* references */
+                    if (stream.match(/^\s*(\&|\*)[a-z0-9\._-]+\b/i)) { 
+                        return 'variable-2'; 
+                    }
 
-//                     /* numbers */
-//                     if (state.inlinePairs == 0 && stream.match(/^\s*-?[0-9\.\,]+\s?$/)) { 
-//                         return 'number';
-//                     }
+                    /* numbers */
+                    if (state.inlinePairs == 0 && stream.match(/^\s*-?[0-9\.\,]+\s?$/)) { 
+                        return 'number';
+                    }
 
-//                     if (state.inlinePairs > 0 && stream.match(/^\s*-?[0-9\.\,]+\s?(?=(,|}))/)) { 
-//                         return 'number'; 
-//                     }
+                    if (state.inlinePairs > 0 && stream.match(/^\s*-?[0-9\.\,]+\s?(?=(,|}))/)) { 
+                        return 'number'; 
+                    }
 
-//                     /* keywords */
-//                     if (stream.match(keywordRegex)) { 
-//                         return 'keyword'; 
-//                     }
-//                 }
+                    /* keywords */
+                    if (stream.match(keywordRegex)) { 
+                        return 'keyword'; 
+                    }
+                }
 
-//                 /* pairs (associative arrays) -> key */
-//                 if (!state.pair && stream.match(/^\s*(?:[,\[\]{}&*!|>'"%@`][^\s'":]|[^,\[\]{}#&*!|>'"%@`])[^#]*?(?=\s*:($|\s))/)) {
-//                     state.pair = true;
-//                     state.keyCol = stream.indentation();
-//                     return "atom";
-//                 }
+                /* pairs (associative arrays) -> key */
+                if (!state.pair && stream.match(/^\s*(?:[,\[\]{}&*!|>'"%@`][^\s'":]|[^,\[\]{}#&*!|>'"%@`])[^#]*?(?=\s*:($|\s))/)) {
+                    state.pair = true;
+                    state.keyCol = stream.indentation();
+                    return "atom";
+                }
 
-//                 if (state.pair && stream.match(/^:\s*/)) { 
-//                     state.pairStart = true; return 'meta'; 
-//                 }
+                if (state.pair && stream.match(/^:\s*/)) { 
+                    state.pairStart = true; return 'meta'; 
+                }
 
-//                 /* nothing found, continue */
-//                 state.pairStart = false;
-//                 state.escaped = (ch == '\\');
-//                 stream.next();
-//                 return null;
-//             },
+                /* nothing found, continue */
+                state.pairStart = false;
+                state.escaped = (ch == '\\');
+                stream.next();
+                return null;
+            },
 
-//             startState: function() {
-//                 return {
-//                     pair: false,
-//                     pairStart: false,
-//                     keyCol: 0,
-//                     inlinePairs: 0,
-//                     inlineList: 0,
-//                     literal: false,
-//                     escaped: false,
-//                     tagLevel: -1,
-//                     tags: [],
-//                 };
-//             }
-//         };
-//     });
+            startState: function() {
+                return {
+                    pair: false,
+                    pairStart: false,
+                    keyCol: 0,
+                    inlinePairs: 0,
+                    inlineList: 0,
+                    literal: false,
+                    escaped: false
+                };
+            },
+            lineComment: "#"
+        };
+    });
 
-//     CodeMirror.defineMIME("text/x-yaml", "yaml");
+    CodeMirror.defineMIME("text/x-yaml", "yaml");
 
-// });
+});
 
 //  YAML-TANGRAM
 //  ===============================================================================
-function yamlAddressing(stream, state) {
-    // if (!state.tags) state.tags = [];
-    // if(state.tagLevel !== undefined) state.tagLevel = -1;
-
-    // Once per line compute the TAGS tree, NAME, ADDRESS and LEVEL.
-    if (stream.pos === 0) {
-        var regex = /(^\s*)(\w+):/gm;
-        var tag = regex.exec(stream.string);
-        if ( tag ){
-
-            //  If looks like a tag
-            //  Calculate the number of spaces and indentation level
-            var spaces = (tag[1].match(/\s/g) || []).length
-            var level = Math.floor( spaces  / stream.tabSize );
-
-            //  Update the TAGS tree
-            if ( level > state.tagLevel ) {
-                state.tags.push(tag[2]);
-            } else if (level === state.tagLevel ) {
-                state.tags[level] = tag[2];
-            } if ( level < state.tagLevel ) {
-                var diff = state.tagLevel - level; 
-                for (var i = 0; i < diff; i++){
-                    state.tags.pop();
-                }
-                state.tags[level] = tag[2];
-            }
-
-            //  Record all that in the state value
-            state.tagName = tag[2];
-            state.tagLevel = level;
-            state.tagAddress = tags2Address(state.tags);
-        }
-    }
-}
-
 (function(mod) {
     if (typeof exports == "object" && typeof module == "object") // CommonJS
         mod(require("../../lib/codemirror"), require("src/codemirror/mode/yaml"), require("src/codemirror/mode/clike"));
@@ -311,14 +280,12 @@ function yamlAddressing(stream, state) {
                     header !== "color" &&
                     header !== "filter" ) {
 
-                    // console.log("shader mode on at " + address);
-                        
                     state.token = glsl;
                     state.localMode = glslMode;
                     state.localState = glslMode.startState(getSpaces(stream.string));
                     return glsl(stream, state);
                 }
-            } 
+            }
             return yamlMode.token(stream, state.yamlState);
         }
 
@@ -334,17 +301,11 @@ function yamlAddressing(stream, state) {
                 header === "color" ||
                 header === "filter" ) {
 
-                // if (stream.pos === 0) console.log("shader mode off at " + address);
-
                 state.token = yaml;
                 state.localState = state.localMode = null;
 
                 return null;
             }
-
-            // if (stream.pos === 0) console.log(stream.string);
-
-            // return yamlMode.token(stream, state);
             return glslMode.token(stream, state.localState);
         }
 
