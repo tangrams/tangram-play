e6120771fc80983f43c05cc2f65a214b18f87441
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-06-17 11:39:29 -0400

    empty color picker click

--- src/cm-tangram/widgets.js
+++ src/cm-tangram/widgets.js
@@ -118,16 +118,7 @@ function updateWidgets(cm){
 }
 
 function colorPickerClicked(div){
-    var cpicker = '<div id="colorpicker" style="font-family:Arial,Helvetica,sans-serif;border:solid 1px #cccccc;position:absolute;width:240px;height:326px;background: #ffffff;-moz-box-shadow:0 0 6px rgba(0,0,0,.25);-webkit-box-shadow: 0 0 6px rgba(0,0,0,.25);box-shadow:0 0 6px rgba(0,0,0,.25);-moz-border-radius: 5px;-webkit-border-radius:5px;border-radius:5px;"><div style="background-color:#2d6ab4;position:absolute;top:0px;left:0px; width:100%; height:22px;text-align:center;padding-top:2px;font-weight:bold;border-top-right-radius:5px;border-top-left-radius:5px;">HTML Color Picker</div><script src="http://widget.colorcodehex.com/color-picker/abcdef.html" type="text/javascript"></script></div>';
-    var input = '<input type="text" style="width: 10em" class="CodeMirror-search-field"/>';
-    editor.openDialog(  cpicker, 
-                        function(){
-                            console.log("hi");
-                        },
-                        {   value: getValue(editor, parseInt(div.value)),
-                            closeOnEnter:true 
-                        } 
-                    );
+
 }
 
 function dropdownMenuChange(select) {
