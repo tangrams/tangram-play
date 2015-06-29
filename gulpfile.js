934551f8412064bc8dd104c6b86f1fc2fd78b102
Lou Huang <lou@louhuang.com>
2015-06-29 11:47:10 -0400

    Make gulpfile pass jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,48 @@
-'use strict'
+'use strict';
 
-var gulp = require('gulp')
-var livereload = require('gulp-livereload')
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
 
 var paths = {
-  styles: 'src/css/**/*'
-}
+    styles: 'src/css/**/*'
+};
 
 gulp.task('css', function () {
-  var postcss = require('gulp-postcss')
-  var cssnext = require('cssnext')
-  var nested = require('postcss-nested')
-  var simplevars = require('postcss-simple-vars')
-  var sourcemaps = require('gulp-sourcemaps')
-  var reporter = require('postcss-reporter')
+    var postcss = require('gulp-postcss');
+    var cssnext = require('cssnext');
+    var nested = require('postcss-nested');
+    var simplevars = require('postcss-simple-vars');
+    var sourcemaps = require('gulp-sourcemaps');
+    var reporter = require('postcss-reporter');
 
-  var options = {
-    browsers: ['last 2 versions', 'IE >= 11'],
-    compress: {
-      comments: { removeAll: true },
-      zindex: false
-    },
-    url: false, // TODO: Figure this out
-    plugins: [
-      nested,
-      simplevars
-    ]
-  }
+    var options = {
+        browsers: ['last 2 versions', 'IE >= 11'],
+        compress: {
+            comments: { removeAll: true },
+            zindex: false
+        },
+        url: false, // TODO: Figure this out
+        plugins: [
+            nested,
+            simplevars
+        ]
+    };
 
-  var plugins = [
-    cssnext(options),
-    reporter()
-  ]
+    var plugins = [
+        cssnext(options),
+        reporter()
+    ];
 
-  return gulp.src('./src/css/main.css')
-    .pipe(sourcemaps.init())
-    .pipe(postcss(plugins))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./css'))
-    .pipe(livereload())
-})
+    return gulp.src('./src/css/main.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss(plugins))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css'))
+        .pipe(livereload());
+});
 
 // Rerun the task when a file changes
 gulp.task('watch', function () {
-  livereload.listen()
-  gulp.watch(paths.styles, ['css'])
-})
+    livereload.listen();
+    gulp.watch(paths.styles, ['css']);
+});
