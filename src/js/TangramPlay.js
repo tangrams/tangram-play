17c788cbfeb79d3ef1217656c80848ee16ae2bc2
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-10-05 14:49:07 -0400

    fixing getKeys function to get the correct keys

--- src/js/TangramPlay.js
+++ src/js/TangramPlay.js
@@ -164,28 +164,38 @@ class TangramPlay {
 
     getKeys(from, to) {
         let keys = [];
-        for (let i = from.line; i <= to.line; i++) {
-            let inLineKeys = this.getKeysOnLine(i);
+
+        if ( from.line === to.line ) {
+            // If the searched keys are in a same line
+            let line = from.line;
+            let inLineKeys = this.getKeysOnLine(line);
 
             for (let key of inLineKeys) {
-                if (key.range.from.line === from.line && key.range.to.line === to.line) {
-                    // Is in the begining line
-                    if (key.range.to.ch > from.ch && key.range.from < to.ch) {
-                        keys.push(key);
-                    }
-                } else if (key.range.from.line === from.line) {
-                    // Is in the begining line
-                    if (key.range.to.ch > from.ch) {
-                        keys.push(key);
-                    }
-                } else if (key.range.to.line === to.line ) {
-                    // is in the end line
-                    if (key.range.from.ch < to.ch) {
-                        keys.push(key);
-                    }
-                } else {
-                    // is in the swandwich lines
+                if (key.range.to.ch > from.ch || key.range.from < to.ch) {
                     keys.push(key);
+                }
+            }
+
+        } else {
+            // If the searched keys are in a range of lines
+            for (let i = from.line; i <= to.line; i++) {
+                let inLineKeys = this.getKeysOnLine(i);
+
+                for (let key of inLineKeys) {
+                    if (key.range.from.line === from.line) {
+                        // Is in the begining line
+                        if (key.range.to.ch > from.ch) {
+                            keys.push(key);
+                        }
+                    } else if (key.range.to.line === to.line ) {
+                        // is in the end line
+                        if (key.range.from.ch < to.ch) {
+                            keys.push(key);
+                        }
+                    } else {
+                        // is in the swandwich lines
+                        keys.push(key);
+                    }
                 }
             }
         }
