dc0d56bac8e656cba93ecf473ee4abb2d1df459f
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-10-05 15:50:27 -0400

    commenting line tracking

--- src/js/addons/WidgetsManager.js
+++ src/js/addons/WidgetsManager.js
@@ -23,6 +23,9 @@ export default class WidgetsManager {
 
         this.data = []; // tokens to check
 
+        this.totalLines = 0; // keep track of linesf
+        this.pairedUntilLine = 0;
+
         // Load data file
         httpGet(configFile, (err, res) => {
             let widgetsData = JSON.parse(res)['values'];
@@ -35,23 +38,42 @@ export default class WidgetsManager {
             }
 
             // Build all widgets
-            this.create({
-                            line:0,
-                            ch:0
-                        },
+            this.create({ line:0, ch:0 },
                         {
                             line: TangramPlay.editor.getDoc().size-1,
                             ch: TangramPlay.editor.getLine(TangramPlay.editor.getDoc().size-1).length
                         });
         });
 
-        // // Suggestions are trigged by the folowing CM events
+        // If something change only update that
         TangramPlay.editor.on('changes', (cm, changesObjs) => {
             for (let obj of changesObjs) {
                 this.clear(obj.from,obj.to);
                 this.create(obj.from,obj.to);
             }
         });
+
+        // Keep track of possible NOT-PARSED lines
+        // and in every codemirror "render update" check if we are aproaching a
+        // non-parsed area and for it to update by cleaning and creating
+        // TangramPlay.editor.on('update', (cm, changesObj) => {
+            
+        //     let to = { line: TangramPlay.editor.getViewport().to-1, ch: TangramPlay.editor.getLine(TangramPlay.editor.getViewport().to-1).length };
+
+        //     if (this.pairedUntilLine < to.line) {
+        //         let from = { line: this.pairedUntilLine+1, ch: 0};
+
+        //         // this.clear(from,to);
+        //         // this.create(from,to);
+
+        //         this.pairedUntilLine = to.line;
+        //     }
+        // });
+
+        // // If a new files is loaded reset the tracked line
+        // TangramPlay.on('url_loaded', (url) => {
+        //     this.pairedUntilLine = 0;
+        // })
     }
 
     create(from, to) {
