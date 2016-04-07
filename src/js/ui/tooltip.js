4bfe3c95dd8ed695800065c64575be2d92c654a2
Lou Huang <lou@louhuang.com>
2016-04-07 15:27:12 -0400

    Refactor tooltip (not a class)

--- src/js/ui/tooltip.js
+++ src/js/ui/tooltip.js
@@ -2,131 +2,126 @@ const TIME_BEFORE_DISPLAY = 500; // in milliseconds, the amount of time elapsed 
 const TIME_BEFORE_RESET = 100; // in milliseconds, the amount of time elapsed after a tooltip disappears that it should appear to animate in instead of appearing instantly
 const VIEWPORT_EDGE_BUFFER = 6; // in pixels, minimum distance the edge of a tooltip should be from edge of the viewport
 
-const Tooltip = {
-    // Declare variables for tooltip lifetime management
-    isCurrentlyShowing: false,
-    displayTimer: null,
-    target: null,
-    el: null,
+let isCurrentlyShowing = false;
+let displayTimer = false;
+let target = null;
+let tooltipEl = null;
+let pointerEl = null;
+let contentEl = null;
 
-    init () {
-        // Create tooltip DOM element and append to page
-        this.el = createDom();
-        document.body.appendChild(this.el);
+initTooltip();
 
-        // Cache references to child elements
-        this.pointerEl = this.el.querySelector('.tooltip-pointer');
-        this.contentEl = this.el.querySelector('.tooltip-content');
+function initTooltip () {
+    // Create tooltip DOM element and append to page
+    tooltipEl = createTooltipDOM();
+    document.body.appendChild(tooltipEl);
 
-        // Look for any tooltips in DOM and attach
-        // event listeners immediately.
-        let els = document.querySelectorAll('[data-tooltip]');
-        for (let el of els) {
-            this.setupTarget(el);
-        }
-    },
+    // Cache references to child elements
+    pointerEl = tooltipEl.querySelector('.tooltip-pointer');
+    contentEl = tooltipEl.querySelector('.tooltip-content');
 
-    setupTarget (target) {
-        // Given a DOM target, set up mouse events on it
-        // This is automatically called on init() for all targets
-        // that already have the [data-tooltip] attribute on it
-        // If you dynamically create elements that require a tooltip,
-        // call this specifically for those elements.
-        target.addEventListener('mouseenter', (e) => {
-            Tooltip.considerShowing(target);
-        }, false);
-
-        target.addEventListener('mouseleave', (e) => {
-            Tooltip.hide();
-        }, false);
-    },
-
-    considerShowing (target) {
-        // Do not show if target has disabled the tooltip
-        if (target.getAttribute('data-tooltip-state') === 'disabled') {
-            return;
-        }
-
-        // Assign current target element
-        this.target = target;
-
-        // If tooltips are already visible, just show the next one immediately
-        if (this.isCurrentlyShowing === true) {
-            this.show();
-        }
-        // Otherwise, there is a slight delay, and an animation
-        else {
-            this.displayTimer = window.setTimeout(() => {
-                this.el.classList.add('tooltip-animate');
-                this.show();
-            }, TIME_BEFORE_DISPLAY);
-        }
-    },
-
-    show () {
-        // Set the tooltip's message and position based on target element
-        this.contentEl.textContent = this.target.getAttribute('data-tooltip');
-        this.applyPosition();
-
-        // Force browsers to end batch reflow computation so that animations work
-        window.setTimeout(() => {
-            window.getComputedStyle(this.el).cssText;
-            this.el.classList.add('tooltip-show');
-        }, 0);
-
-        // Set tooltip management state
-        this.isCurrentlyShowing = true;
-
-        // Set global listener to hide a tooltip on click
-        this._globalEventHandler = (e) => {
-            this.hide();
-        };
-        window.addEventListener('click', this._globalEventHandler, false);
-    },
-
-    hide () {
-        // Better to clear this timer on hide
-        window.clearTimeout(this.displayTimer);
-
-        this.el.classList.remove('tooltip-show');
-
-        // Reset active state after some amount of time, if it is not being shown elsewhere
-        window.setTimeout(() => {
-            if (!this.el.classList.contains('tooltip-show')) {
-                this.isCurrentlyShowing = false;
-            }
-        }, TIME_BEFORE_RESET);
-
-        window.removeEventListener('click', this._globalEventHandler, false);
-    },
-
-    applyPosition () {
-        const targetPos = this.target.getBoundingClientRect();
-        const alignment = this.target.getAttribute('data-tooltip-alignment') || 'left';
-
-        if (alignment === 'right') {
-            let rightXPos = (targetPos.right + VIEWPORT_EDGE_BUFFER < window.innerWidth) ? targetPos.right : (window.innerWidth - VIEWPORT_EDGE_BUFFER);
-            this.el.style.right = Math.floor(window.innerWidth - rightXPos) + 'px';
-            this.el.style.left = 'auto';
-            this.pointerEl.classList.add('tooltip-pointer-right');
-        }
-        else {
-            let leftXPos = (targetPos.left - VIEWPORT_EDGE_BUFFER > 0) ? targetPos.left : VIEWPORT_EDGE_BUFFER;
-            this.el.style.left = Math.floor(leftXPos) + 'px';
-            this.el.style.right = 'auto';
-            this.pointerEl.classList.remove('tooltip-pointer-right');
-        }
-
-        this.el.style.top = Math.floor(targetPos.bottom + 5) + 'px'; // Starting position, animate down
+    // Look for any tooltips in DOM and attach
+    // event listeners immediately.
+    let els = document.querySelectorAll('[data-tooltip]');
+    for (let el of els) {
+        setupTooltipTarget(el);
     }
-};
+}
 
-export default Tooltip;
+function setupTooltipTarget (target) {
+    // Given a DOM target, set up mouse events on it
+    // This is automatically called on init() for all targets
+    // that already have the [data-tooltip] attribute on it
+    // If you dynamically create elements that require a tooltip,
+    // call this specifically for those elements.
+    target.addEventListener('mouseenter', (e) => {
+        considerShowingTooltip(target);
+    }, false);
 
-function createDom () {
-    let dom = document.createElement('div');
-    let tooltipEl = document.createElement('div');
-    let pointerEl = document.createElement('div');
+    target.addEventListener('mouseleave', (e) => {
+        hideTooltip();
+    }, false);
+}
+
+function considerShowingTooltip (el) {
+    // Do not show if target element has disabled the tooltip
+    if (el.getAttribute('data-tooltip-state') === 'disabled') {
+        return;
+    }
+
+    // Assign current target element
+    target = el;
+
+    // If tooltips are already visible, just show the next one immediately
+    if (isCurrentlyShowing === true) {
+        showTooltip();
+    }
+    // Otherwise, there is a slight delay, and an animation
+    else {
+        displayTimer = window.setTimeout(() => {
+            tooltipEl.classList.add('tooltip-animate');
+            showTooltip();
+        }, TIME_BEFORE_DISPLAY);
+    }
+}
+
+function showTooltip () {
+    // Set the tooltip's message and position based on target element
+    contentEl.textContent = target.getAttribute('data-tooltip');
+    applyTooltipPosition();
+
+    // Force browsers to end batch reflow computation so that animations work
+    window.setTimeout(() => {
+        window.getComputedStyle(tooltipEl).cssText;
+        tooltipEl.classList.add('tooltip-show');
+    }, 0);
+
+    // Set tooltip management state
+    isCurrentlyShowing = true;
+
+    window.addEventListener('click', hideTooltip, false);
+}
+
+function hideTooltip () {
+    // Better to clear this timer on hide
+    window.clearTimeout(displayTimer);
+
+    tooltipEl.classList.remove('tooltip-show');
+
+    // Reset active state after some amount of time, if it is not being shown elsewhere
+    window.setTimeout(() => {
+        if (!tooltipEl.classList.contains('tooltip-show')) {
+            isCurrentlyShowing = false;
+        }
+    }, TIME_BEFORE_RESET);
+
+    window.removeEventListener('click', hideTooltip, false);
+}
+
+function applyTooltipPosition () {
+    const targetPos = target.getBoundingClientRect();
+    const alignment = target.getAttribute('data-tooltip-alignment') || 'left';
+
+    if (alignment === 'right') {
+        let rightXPos = (targetPos.right + VIEWPORT_EDGE_BUFFER < window.innerWidth) ? targetPos.right : (window.innerWidth - VIEWPORT_EDGE_BUFFER);
+        tooltipEl.style.right = Math.floor(window.innerWidth - rightXPos) + 'px';
+        tooltipEl.style.left = 'auto';
+        pointerEl.classList.add('tooltip-pointer-right');
+    }
+    else {
+        let leftXPos = (targetPos.left - VIEWPORT_EDGE_BUFFER > 0) ? targetPos.left : VIEWPORT_EDGE_BUFFER;
+        tooltipEl.style.left = Math.floor(leftXPos) + 'px';
+        tooltipEl.style.right = 'auto';
+        pointerEl.classList.remove('tooltip-pointer-right');
+    }
+
+    tooltipEl.style.top = Math.floor(targetPos.bottom + 5) + 'px'; // Starting position, animate down
+}
+
+function createTooltipDOM () {
+    const dom = document.createElement('div');
+    const tooltipEl = document.createElement('div');
+    const pointerEl = document.createElement('div');
 
     tooltipEl.className = 'tooltip-content';
     pointerEl.className = 'tooltip-pointer';
--- src/js/ui/ui.js
+++ src/js/ui/ui.js
@@ -1,13 +1,12 @@
 import '../file/file-drop';
 import { initDivider } from './divider';
 import Menu from '../menus/menu';
-import Tooltip from './tooltip';
+import './tooltip';
 import MapToolbar from '../map/toolbar';
 
 export default class UI {
     constructor () {
         // Set up UI components
-        Tooltip.init();
         MapToolbar.init();
 
         initDivider();
