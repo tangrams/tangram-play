e9c05a895b63eefb7d557a67e63f503fc2eccc57
Lou Huang <lou@louhuang.com>
2016-04-08 19:07:24 -0400

    Begin smarter "load from Gist" flow

--- src/js/modals/modal.open-url.js
+++ src/js/modals/modal.open-url.js
@@ -1,6 +1,7 @@
 import TangramPlay from '../tangram-play';
 import Modal from './modal';
 import EditorIO from '../editor/io';
+import { parseForGistURL, isGistURL } from '../tools/gist-url';
 
 let modalEl;
 
@@ -12,7 +13,12 @@ class OpenUrlModal extends Modal {
         this.message = 'Open a scene file from URL';
         this.input = this.el.querySelector('.open-url-input input');
         this.input.addEventListener('keyup', (event) => {
-            if (this.input.value && this.input.validity.valid === true && this.input.value.match(/\.y(a?)ml$/)) {
+            // Check for valid URL.
+            // Reported as valid by the form element AND
+            // either ends with a YAML extension or matches a Gist URL
+            if (this.input.value && this.input.validity.valid === true &&
+                (this.input.value.match(/\.y(a?)ml$/) ||
+                isGistURL(this.input.value))) {
                 this.el.querySelector('.modal-confirm').removeAttribute('disabled');
                 let key = event.keyCode || event.which;
                 if (key === 13) {
@@ -25,7 +31,21 @@ class OpenUrlModal extends Modal {
         });
 
         this.onConfirm = () => {
-            const value = this.input.value;
+            let value = this.input.value.trim();
+            value = parseForGistURL(value);
+            // TODO: If it's a Gist URL:
+            // Grab the manifest from the API. (We'll call the response object "gist")
+            // Iterate through gist.files, an object whose keys are the filenames of each file.
+            // Find the first file with type "text/x-yaml".
+            //      gist.files[file].type === 'text/x-yaml'
+            // In the future, we will have to be smarter than this -- there might be
+            // multiple files, or it might be in a different format. But for now,
+            // we assume there's one Tangram YAML file and that the MIME-type is correct.
+            // Grab that file's raw_url property and read it in.
+            // Alternatively, the content property might be read directly (if truncated is false)?
+            // Because this requires several successive API calls, we need to wrap
+            // TangramPlay.load() in a Promise wrapper.
+
             this.clearInput();
             TangramPlay.load({ url: value });
         };
---
+++ src/js/tools/gist-url.js
@@ -0,0 +1,47 @@
+/**
+ * GitHub Gist URL parsing
+ *
+ * Given a URL string, detect if the URL is a GitHub Gist URL. If so,
+ * return the link to the raw YAML form for Tangram Play.
+ * If it is not a GitHub Gist URL, pass the original input through.
+ */
+
+const gistIdRegexp = /\/\/(?:(?:gist.github.com|gist.githubusercontent.com)(?:\/[A-Za-z0-9_-]+){0,1}|api.github.com\/gists)\/([a-z0-9]+)(?:$|\/|.)/;
+
+export function parseForGistURL (value) {
+  if (isGistURL(value) === true) {
+    // The last capture group of the RegExp should be the gistID
+    const gistId = value.match(gistIdRegexp).pop();
+    return 'https://api.github.com/gists/' + gistId;
+  } else {
+    return value;
+  }
+}
+//https://gist.githubusercontent.com/anonymous/6cd74ed889519d7426179ceec1e287d7/raw/2694028ba2263a79b450e1fab14bc2a02839fc1d/scene.yaml
+/**
+ * Is this a generic GitHub Gist URL?
+ * This should test whether the URL provided matches signatures of:
+ *
+ *    https://gist.github.com/user/gistId
+ *    https://gist.github.com/gistId
+ *    https://api.github.com/gists/gistId
+ *    https://gist.githubusercontent.com/user/gistId
+ *    https://gist.githubusercontent.com/gistId
+ *
+ * gistIds are alphanumeric strings but do not have fixed length.
+ * Each of the urls above may be followed with a forward slash
+ * or a period (for ".git")
+ *
+ * If the URL matches, it returns true. Otherwise, this returns false.
+ * Note that this does not test whether the URL is actually a valid URL
+ * string or that the gist exists.
+ */
+export function isGistURL (value) {
+  if ((value.includes('api.github.com/gists') || value.includes('gist.github.com')) &&
+    value.match(gistIdRegexp).length > 1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
