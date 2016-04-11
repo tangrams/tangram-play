fc76947d5b4d2b7dbf44d565c0b201a254b7dec7
Lou Huang <lou@louhuang.com>
2016-04-11 15:03:41 -0400

    Update documentation on gist url parsing

--- src/js/tools/gist-url.js
+++ src/js/tools/gist-url.js
@@ -1,34 +1,31 @@
 /**
  * GitHub Gist URL parsing
- *
- * Given a URL string, detect if the URL is a GitHub Gist URL. If so,
- * return the link to the raw YAML form for Tangram Play.
- * If it is not a GitHub Gist URL, pass the original input through.
  */
 
+// This regular expression tests whether the URL provided
+// matches the following signatures:
+//
+//    https://gist.github.com/{user}/{gistId}
+//    https://gist.github.com/{gistId}
+//    https://api.github.com/gists/{gistId}
+//    https://gist.githubusercontent.com/{user}/{gistId}
+//    https://gist.githubusercontent.com/{gistId}
+//
+// gistIds are alphanumeric strings but do not have a fixed length.
+// Each of the urls above may be followed with a forward slash
+// or a period (for ".git")
 const gistIdRegexp = /\/\/(?:(?:gist.github.com|gist.githubusercontent.com)(?:\/[A-Za-z0-9_-]+){0,1}|api.github.com\/gists)\/([a-z0-9]+)(?:$|\/|.)/;
 
 /**
  * Is this a generic GitHub Gist URL?
- * This should test whether the URL provided matches signatures of:
- *
- *    https://gist.github.com/user/gistId
- *    https://gist.github.com/gistId
- *    https://api.github.com/gists/gistId
- *    https://gist.githubusercontent.com/user/gistId
- *    https://gist.githubusercontent.com/gistId
- *
- * gistIds are alphanumeric strings but do not have fixed length.
- * Each of the urls above may be followed with a forward slash
- * or a period (for ".git")
  *
  * If the URL matches, it returns true. Otherwise, this returns false.
  * Note that this does not test whether the URL is actually a valid URL
  * string or that the gist exists.
  */
-export function isGistURL (value) {
-  if ((value.includes('api.github.com/gists') || value.includes('gist.github.com')) &&
-    value.match(gistIdRegexp).length > 1) {
+export function isGistURL (url) {
+  if ((url.includes('api.github.com/gists') || url.includes('gist.github.com')) &&
+    url.match(gistIdRegexp).length > 1) {
     return true;
   }
   else {
@@ -36,9 +33,17 @@ export function isGistURL (value) {
   }
 }
 
-export function getGistURL (value) {
+/**
+ * Given any Gist URL (it should pass the isGistURL() test, above),
+ * we return the URL endpoint for the "Get a single gist" API.
+ * See: https://developer.github.com/v3/gists/#get-a-single-gist
+ * The response from this API endpoint will tell us everything we need
+ * to know to load a Gist, so all Gist URLs must convert to this
+ * format:
+ *    https://api.github.com/gists/{gistId}
+ */
+export function getGistURL (url) {
   // The last capture group of the RegExp should be the gistID
-  const gistId = value.match(gistIdRegexp).pop();
+  const gistId = url.match(gistIdRegexp).pop();
   return 'https://api.github.com/gists/' + gistId;
 }
-//https://gist.githubusercontent.com/anonymous/6cd74ed889519d7426179ceec1e287d7/raw/2694028ba2263a79b450e1fab14bc2a02839fc1d/scene.yaml
