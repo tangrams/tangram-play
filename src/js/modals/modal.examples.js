84b4845a1cba0b19d82ddf06cdd9ce2aa4a62257
Lou Huang <lou@louhuang.com>
2016-04-13 19:00:55 -0400

    Refactor examples modal

--- src/js/modals/modal.examples.js
+++ src/js/modals/modal.examples.js
@@ -2,29 +2,24 @@ import TangramPlay from '../tangram-play';
 import Modal from './modal';
 import EditorIO from '../editor/io';
 
-let examplesEl;
-
 class ExamplesModal extends Modal {
     constructor (config) {
-        super();
-
-        // TODO: Load this another way?
-        const CONFIG_FILE = 'data/menu.json';
-
-        this.el = examplesEl = document.body.querySelector('.example-modal');
-        this.message = 'Choose an example to open';
-        loadExamples(CONFIG_FILE);
-
-        this.onConfirm = () => {
+        const message = 'Choose an example to open';
+        const onConfirm = () => {
             const selected = this.el.querySelectorAll('.example-option.example-selected')[0];
-            const value = selected.getAttribute('data-value');
-            TangramPlay.load({ url: value });
+            const url = selected.getAttribute('data-value');
+            TangramPlay.load({ url });
         };
-
-        this.onAbort = () => {
-            resetExampleSelection();
+        const onAbort = () => {
+            this._resetExampleSelection();
             this.el.querySelector('.modal-confirm').disabled = true;
         };
+
+        super(message, onConfirm, onAbort, {
+            el: document.body.querySelector('.example-modal')
+        });
+
+        this._loadExamples();
     }
 
     show () {
@@ -32,78 +27,82 @@ class ExamplesModal extends Modal {
             super.show();
         });
     }
+
+    // TODO: Refactor
+    _loadExamples () {
+        const CONFIG_FILE = 'data/menu.json';
+
+        window.fetch(CONFIG_FILE, { credentials: 'include' })
+            .then(response => {
+                if (response.status !== 200) {
+                    throw response.status;
+                }
+
+                return response.json();
+            })
+            .then(data => {
+                const listEl = this.el.querySelector('.example-list');
+
+                data.examples.forEach(category => {
+                    const categoryHeaderEl = document.createElement('h2');
+                    const categoryUnderlineEl = document.createElement('hr');
+
+                    categoryHeaderEl.className = 'example-list-header';
+                    categoryHeaderEl.textContent = category.category;
+
+                    listEl.appendChild(categoryHeaderEl);
+                    listEl.appendChild(categoryUnderlineEl);
+
+                    category.scenes.forEach(scene => {
+                        const newOption = document.createElement('div');
+                        const nameEl = document.createElement('div');
+                        const thumbnailEl = document.createElement('div');
+
+                        newOption.className = 'example-option';
+                        newOption.setAttribute('data-value', scene.url);
+
+                        nameEl.className = 'example-option-name';
+                        nameEl.textContent = scene.name;
+
+                        thumbnailEl.className = 'example-thumbnail';
+                        thumbnailEl.style.backgroundColor = 'rgba(255,255,255,0.05)';
+                        thumbnailEl.style.backgroundImage = 'url(' + scene.thumb + ')';
+
+                        newOption.appendChild(nameEl);
+                        newOption.appendChild(thumbnailEl);
+                        newOption.addEventListener('click', event => {
+                            this._selectExample(event.target);
+                        });
+
+                        listEl.appendChild(newOption);
+
+                        newOption.addEventListener('dblclick', event => {
+                            this._handleConfirm();
+                        });
+                    });
+                });
+            })
+            .catch(error => {
+                console.error('Error retrieving config file.', error);
+            });
+    }
+
+    _selectExample (target) {
+        while (!target.classList.contains('example-option')) {
+            target = target.parentNode;
+        }
+        this._resetExampleSelection();
+        target.classList.add('example-selected');
+        this.el.querySelector('.modal-confirm').disabled = false;
+    }
+
+    _resetExampleSelection () {
+        const allExamples = this.el.querySelectorAll('.example-option');
+        for (let example of allExamples) {
+            example.classList.remove('example-selected');
+        }
+    }
 }
 
 export const examplesModal = new ExamplesModal();
 
-// TODO: Refactor
-function loadExamples (configFile) {
-    window.fetch(configFile, { credentials: 'include' })
-        .then((response) => {
-            if (response.status !== 200) {
-                throw response.status;
-            }
-
-            return response.json();
-        })
-        .then((data) => {
-            const listEl = examplesEl.querySelector('.example-list');
-
-            data.examples.forEach(category => {
-                const categoryHeaderEl = document.createElement('h2');
-                const categoryUnderlineEl = document.createElement('hr');
-
-                categoryHeaderEl.className = 'example-list-header';
-                categoryHeaderEl.textContent = category.category;
-
-                listEl.appendChild(categoryHeaderEl);
-                listEl.appendChild(categoryUnderlineEl);
-
-                category.scenes.forEach(scene => {
-                    const newOption = document.createElement('div');
-                    const nameEl = document.createElement('div');
-                    const thumbnailEl = document.createElement('div');
-
-                    newOption.className = 'example-option';
-                    newOption.setAttribute('data-value', scene.url);
-
-                    nameEl.className = 'example-option-name';
-                    nameEl.textContent = scene.name;
-
-                    thumbnailEl.className = 'example-thumbnail';
-                    thumbnailEl.style.backgroundColor = 'rgba(255,255,255,0.05)';
-                    thumbnailEl.style.backgroundImage = 'url(' + scene.thumb + ')';
-
-                    newOption.appendChild(nameEl);
-                    newOption.appendChild(thumbnailEl);
-                    newOption.addEventListener('click', selectExample);
-
-                    listEl.appendChild(newOption);
-
-                    newOption.addEventListener('dblclick', function (e) {
-                        examplesModal._handleConfirm();
-                    });
-                });
-            });
-        })
-        .catch((error) => {
-            console.error('Error retrieving config file.', error);
-        });
-}
-
-function selectExample (event) {
-    let target = event.target;
-    while (!target.classList.contains('example-option')) {
-        target = target.parentNode;
-    }
-    resetExampleSelection();
-    target.classList.add('example-selected');
-    examplesEl.querySelector('.modal-confirm').disabled = false;
-}
-
-function resetExampleSelection () {
-    const allExamples = examplesEl.querySelectorAll('.example-option');
-    for (let example of allExamples) {
-        example.classList.remove('example-selected');
-    }
-}
