bc2c3a24f367f8c73d2f9310569af2ccf8d5a0e7
Lou Huang <lou@louhuang.com>
2016-04-20 10:52:04 -0400

    Add tests for Gist url parsing.

--- test/karma.spec.js
+++ test/karma.spec.js
@@ -1,19 +1,22 @@
+// Polyfills ES2015 features, like Promises, globally.
+// This only needs to be done once for the entire test environment.
 import 'babel-polyfill';
+
 import chai, { assert } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 
 chai.use(chaiAsPromised);
 
 describe('Karma test with Chai', function () {
-    it('should expose the Chai assert method', function () {
+    it('exposes the Chai assert method', function () {
         assert.isOk('everything', 'everything is ok');
     });
 
-    it('should work with ES6 fat arrow function', () => {
+    it('works with ES6 fat arrow function', () => {
         assert.ok(true, 'this is ok');
     });
 
-    it('should extend assert with support for Promises', function () {
+    it('extends assert with support for Promises', function () {
         return assert.eventually.equal(Promise.resolve(2 + 2), 4, 'This had better be true, eventually');
     });
 });
