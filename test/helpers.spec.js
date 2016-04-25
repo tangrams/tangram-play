885d0bb28452ce0eeb0f527d67fd03e0dfa29ee5
Lou Huang <lou@louhuang.com>
2016-04-24 21:53:54 -0700

    Relative urls should not have prepended protocols.

--- test/helpers.spec.js
+++ test/helpers.spec.js
@@ -25,5 +25,21 @@ describe('Helpers', () => {
             assert.strictEqual(prependProtocolToUrl(test3), test3);
             assert.strictEqual(prependProtocolToUrl(test4), test4);
         });
+
+        it('ignores urls that should be relative links', () => {
+            const test1 = 'data/scenes/empty.yaml';
+            const test2 = 'foo.yaml';
+            const test3 = '/foo.yaml';
+            const test4 = './foo.yaml';
+            const test5 = '../foo/bar.yaml';
+            const test6 = 'foo.yaml?bar'; // Filename with query string (e.g. for cache-busting)
+
+            assert.strictEqual(prependProtocolToUrl(test1), test1);
+            assert.strictEqual(prependProtocolToUrl(test2), test2);
+            assert.strictEqual(prependProtocolToUrl(test3), test3);
+            assert.strictEqual(prependProtocolToUrl(test4), test4);
+            assert.strictEqual(prependProtocolToUrl(test5), test5);
+            assert.strictEqual(prependProtocolToUrl(test6), test6);
+        });
     });
 });
