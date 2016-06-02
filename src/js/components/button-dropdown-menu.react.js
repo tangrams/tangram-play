1459c30dcf5a54b082f949114e0e752d575039fa
Irene Alvarado <ire.alvarado@gmail.com>
2016-06-02 10:48:52 -0400

    Further broke down menu components into modular buttons

---
+++ src/js/components/button-dropdown-menu.react.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Dropdown from 'react-bootstrap/lib/Dropdown';
+import DropdownMenu from 'react-bootstrap/lib/DropdownMenu';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+
+import Icon from './icon.react';
+
+export default React.createClass({
+    render: function() {
+        var results = this.props.menu_items;
+        return (
+            <Dropdown.Menu {...this.props} bsClass="dropdown">
+                {results.map(function(result) {
+                  return <MenuItem bsClass="test" key={result.id}><Icon type={result.icon} />{result.text}</MenuItem>;
+                })}
+            </Dropdown.Menu>
+
+            );
+    }
+
+});
--- src/js/components/left-menu.react.js
+++ src/js/components/left-menu.react.js
@@ -3,13 +3,10 @@ import React from 'react';
 import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Dropdown from 'react-bootstrap/lib/Dropdown';
-import MenuItem from 'react-bootstrap/lib/MenuItem';
-import Tooltip from 'react-bootstrap/lib/Tooltip';
-import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 
-import SimpleButton from './simple-button.react';
-import DropdownButton from './dropdown-button.react';
-import Icon from './icon.react';
+import ButtonSimple from './button-simple.react';
+import ButtonDropdownToggle from './button-dropdown-toggle.react';
+import ButtonDropdownMenu from './button-dropdown-menu.react';
 import EditorIO from '../editor/io';
 
 export default React.createClass({
@@ -17,27 +14,40 @@ export default React.createClass({
         return (
             <ButtonToolbar>
                 <ButtonGroup>
-                    <SimpleButton button_text={"New"} overlay_text={"New Scene"} icon={"bt-file"} click={someFn} />
-                    <SimpleButton button_text={"Open"} overlay_text={"Open Scene"} icon={"bt-upload"} />
+                    <ButtonSimple button_text={"New"} overlay_text={"New Scene"} icon={"bt-file"} click={someFn} />
+                    <ButtonSimple button_text={"Open"} overlay_text={"Open Scene"} icon={"bt-upload"} />
                 </ButtonGroup>
 
                 <ButtonGroup>
-                    <Dropdown id="dropdown-left" >
-                        <DropdownButton bsRole="toggle" text="test" icon="bt-upload">
-                        </DropdownButton>
-                      <Dropdown.Menu bsClass="test">
-                        <MenuItem bsClass="test"><Icon type={"bt-file"} />Save to file</MenuItem>
-                        <MenuItem ><Icon type={"bt-code"} />Save to Gist</MenuItem>
-                        <MenuItem ><Icon type={"bt-camera"} />Active Item</MenuItem>
-                      </Dropdown.Menu>
+                    <Dropdown id="dropdown-save" >
+                        <ButtonDropdownToggle bsRole="toggle" text="Save" icon="bt-download">
+                        </ButtonDropdownToggle>
+                        <ButtonDropdownMenu bsRole="menu" menu_items={save_menu}>
+                        </ButtonDropdownMenu>
                     </Dropdown>
-
-                    </ButtonGroup>
+                </ButtonGroup>
             </ButtonToolbar>
         );
-    },
-
+    }
 });
+
+let save_menu = [
+    {
+        id: 1,
+        text: "Save to file",
+        icon: "bt-folder"
+    },
+    {
+        id: 2,
+        text: "Save to Gist",
+        icon: "bt-code"
+    },
+    {
+        id: 3,
+        text: "Take a screenshot",
+        icon: "bt-camera"
+    }
+] ;
 
 let someFn = function() {
     console.log('new clicked');
