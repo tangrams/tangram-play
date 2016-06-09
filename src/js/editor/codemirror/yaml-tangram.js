ddde97985f2fe4c2b80105c373882d279db85bb9
Lou Huang <lou@louhuang.com>
2016-06-09 17:03:22 -0400

    Refactor and document shader block checking functions

--- src/js/editor/codemirror/yaml-tangram.js
+++ src/js/editor/codemirror/yaml-tangram.js
@@ -93,38 +93,61 @@ export function getAddressForLevel (address, level) {
 //  CHECK
 //  ===============================================================================
 
-//  Function that check if a line is inside a Color Shader Block
-function isGlobalBlock (address) {
-    return _.endsWith(address, 'shaders:blocks:global');
-}
-function isWidthBlock (address) {
-    return _.endsWith(address, 'shaders:blocks:width');
-}
-function isPositionBlock (address) {
-    return _.endsWith(address, 'shaders:blocks:position');
-}
-export function isNormalBlock (address) {
-    return _.endsWith(address, 'shaders:blocks:normal');
-}
-export function isColorBlock (address) {
-    return _.endsWith(address, 'shaders:blocks:color');
-}
-function isFilterBlock (address) {
-    return _.endsWith(address, 'shaders:blocks:filter');
-}
+/**
+ * Detects if an address refers to a shader block. In Tangram syntax, six
+ * blocks are defined. https://mapzen.com/documentation/tangram/shaders/#blocks
+ *
+ * A valid shader address will always begin with `styles:` and end in
+ * `:shaders:blocks:__block__`, where __block__ is one of the six defined
+ * shader blocks.
+ *
+ * @param {string} address - the address of the key-value pair
+ * @return {Boolean} bool - `true` if address is a shader block
+ */
 function isShader (address) {
-    return (
-        isGlobalBlock(address) ||
-        isWidthBlock(address) ||
-        isPositionBlock(address) ||
-        isNormalBlock(address) ||
-        isColorBlock(address) ||
-        isFilterBlock(address)
-    );
+    const re = /shaders:blocks:(global|width|position|normal|color|filter)$/;
+    return _.startsWith(address, 'styles') && re.test(address);
 }
 
 /**
- * Detects if a key's contents (value) is JavaScript. In Tangram syntax,
+ * The following functions are very similar and detects if an address refers to
+ * a specific shader block. These are exported as helpers for other modules
+ * to use. See documentation for types:
+ * https://mapzen.com/documentation/tangram/shaders/#blocks
+ *
+ * A valid shader address will always begin with `styles:` and end in
+ * `:shaders:blocks:__block__`, where __block__ is one of the six defined
+ * shader blocks.
+ *
+ * @param {string} address - the address of the key-value pair
+ * @return {Boolean} bool - `true` if address is a shader block of that type
+ */
+export function isGlobalBlock (address) {
+    return _.startsWith(address, 'styles') && _.endsWith(address, 'shaders:blocks:global');
+}
+
+export function isWidthBlock (address) {
+    return _.startsWith(address, 'styles') && _.endsWith(address, 'shaders:blocks:width');
+}
+
+export function isPositionBlock (address) {
+    return _.startsWith(address, 'styles') && _.endsWith(address, 'shaders:blocks:position');
+}
+
+export function isNormalBlock (address) {
+    return _.startsWith(address, 'styles') && _.endsWith(address, 'shaders:blocks:normal');
+}
+
+export function isColorBlock (address) {
+    return _.startsWith(address, 'styles') && _.endsWith(address, 'shaders:blocks:color');
+}
+
+export function isFilterBlock (address) {
+    return _.startsWith(address, 'styles') && _.endsWith(address, 'shaders:blocks:filter');
+}
+
+/**
+ * Detects if an address's contents (value) is JavaScript. In Tangram syntax,
  * JavaScript values must be a single function. This makes detection easy:
  * See if the beginning of the string starts with a valid function declaration.
  *
@@ -140,7 +163,9 @@ function isContentJS (tangramScene, address) {
         // Where whitespace is allowed, it can be any amount of whitespace.
         // Content may begin with a pipe "|" character for YAML multi-line
         // strings. Next, test if "function () {" (with opening brace).
-        return /\s*[\|]*\s*function\s*\(\s*\)\s*\{/gm.test(content);
+        const re = /^\s*\|?\s*function\s*\(\s*\)\s*\{/m;
+
+        return re.test(content);
     }
     else {
         return false;
