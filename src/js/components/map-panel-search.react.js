01d5c1dc8324d9d0db1cf96b3c235a052c411eb9
Irene Alvarado <ire.alvarado@gmail.com>
2016-06-14 15:55:42 -0400

    Added functionality for autocomplete search bar

--- src/js/components/map-panel-search.react.js
+++ src/js/components/map-panel-search.react.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
+import Autosuggest from 'react-autosuggest';
 import Button from 'react-bootstrap/lib/Button';
-import Panel from 'react-bootstrap/lib/Panel';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import Tooltip from 'react-bootstrap/lib/Tooltip';
@@ -14,28 +14,82 @@ import { config } from '../config';
 
 const SEARCH_THROTTLE = 300; // in ms, time to wait before repeating a request
 let latlngLabelPrecision = 4;
+let maxReqTimestampRendered = new Date().getTime();
 
-let makeRequest = debounce(function (endpoint) {
-    httpGet(endpoint, (err, res) => {
-        if (err) {
-            console.error(err);
-        }
-        else {
-            console.log(JSON.parse(res)) ;
-            //showResults(JSON.parse(res));
-        }
-    });
-}, SEARCH_THROTTLE);
+function getSuggestionValue(suggestion) {
+    console.log("Suggestions is: " + suggestions)
+    return suggestion.name ;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion}</span>
+  );
+}
 
 export default class MapPanelSearch extends React.Component {
+    makeRequest(endpoint) {
+        debounce(httpGet(endpoint, (err, res) => {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                this.showResults(JSON.parse(res));
+            }
+        }), SEARCH_THROTTLE);
+    }
+
+    showResults(results) {
+        const features = results.features;
+
+        // // Ignore requests that started before a request which has already
+        // // been successfully rendered on to the UI.
+        // if (results.geocoding.timestamp < maxReqTimestampRendered) {
+        //     return;
+        // }
+        //
+        // // Store the latest timestamp of the last request
+        // maxReqTimestampRendered = results.geocoding.timestamp;
+
+        let list = [] ;
+        for (let i = 0, j = features.length; i < j; i++) {
+            list.push(features[i].properties.label) ;
+        }
+
+        this.setState({
+          results: features,
+          suggestions: list
+        });
+
+    }
+
+    // Get autocomplete suggestions
+    autocomplete(query) {
+        const center = map.getCenter();
+        const endpoint = `//${config.SEARCH.HOST}/v1/autocomplete?text=${query}&focus.point.lat=${center.lat}&focus.point.lon=${center.lng}&layers=coarse&api_key=${config.SEARCH.API_KEY}`;
+        this.makeRequest(endpoint);
+    }
+
+
+    getSuggestions(value) {
+        const query = value.trim() ;
+        if (query.length >= 2) {
+            this.autocomplete(value) ;
+        }
+    }
+
     constructor(props) {
         super(props);
         this.state = {
             location: 'Cuartos, Mexico',
-            latlng: '40.7148, -73.9218'
+            latlng: '40.7148, -73.9218',
+            value: '',
+            suggestions: [],
+            results: []
         };
 
-        this.handleChange = this.handleChange.bind(this);
+        this.onChange = this.onChange.bind(this);
+        //this.onSuggestionsUpdateRequested = this.onSuggestionsUpdateRequested.bind(this);
     }
 
     componentDidMount() {
@@ -69,28 +123,31 @@ export default class MapPanelSearch extends React.Component {
         }), SEARCH_THROTTLE);
     }
 
-    handleChange(e) {
-        var query = e.target.value.trim() ;
-        this.setState({location: query})
-        if (query.length >= 2) {
-            this.autocomplete(query);
-        }
-    }
+    onChange(event, { newValue }) {
+        this.setState({
+          value: newValue
+        });
 
-    // Get autocomplete suggestions
-    autocomplete(query) {
-        const center = map.getCenter();
-        const endpoint = `//${config.SEARCH.HOST}/v1/autocomplete?text=${query}&focus.point.lat=${center.lat}&focus.point.lon=${center.lng}&layers=coarse&api_key=${config.SEARCH.API_KEY}`;
-        makeRequest(endpoint);
+        this.getSuggestions(newValue) ;
     }
 
     render() {
+        const { value, suggestions } = this.state;
+        const inputProps = {
+          placeholder: 'Cuartos, Mexico',
+          value,
+          onChange: this.onChange
+        };
+
         return (
             <ButtonGroup id='buttons-search'>
                 <OverlayTrigger placement='bottom' overlay={<Tooltip id='tooltip'>{'Search for a location'}</Tooltip>}>
                     <Button> <Icon type={'bt-search'} /> </Button>
                 </OverlayTrigger>
-                <input className='map-search-input' onChange={this.handleChange} placeholder={this.state.location} spellcheck='false'></input>
+                <Autosuggest className='map-search-input' suggestions={suggestions}
+                   getSuggestionValue={getSuggestionValue}
+                   renderSuggestion={renderSuggestion}
+                   inputProps={inputProps} />
                 <div className='map-search-latlng'>{this.state.latlng}</div>
                 <OverlayTrigger placement='bottom' overlay={<Tooltip id='tooltip'>{'Bookmark location'}</Tooltip>}>
                     <Button> <Icon type={'bt-star'} /> </Button>
