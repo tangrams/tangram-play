7defe485be18bfdd9b238aec0c1b6c1bdd2dde05
Irene Alvarado <ire.alvarado@gmail.com>
2016-06-14 11:27:36 -0400

    UI for map panel is ready

--- src/js/components/map-panel.react.js
+++ src/js/components/map-panel.react.js
@@ -7,19 +7,64 @@ import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import Tooltip from 'react-bootstrap/lib/Tooltip';
 import Icon from './icon.react';
 
+import LocalStorage from '../storage/localstorage';
+import { map } from '../map/map';
+import search from '../map/search';
+import { initGeolocator } from '../map/geolocator';
+import bookmarks from '../map/bookmarks';
+
+const STORAGE_DISPLAY_KEY = 'map-toolbar-display';
+const MAP_UPDATE_DELTA = 0.002;
+
+let el;
+let currentLocation;
+
+function setZoomLabel () {
+    let label = el.querySelector('.map-zoom-quantity');
+    let currentZoom = map.getZoom();
+    let fractionalNumber = Math.floor(currentZoom * 10) / 10;
+    label.textContent = fractionalNumber.toFixed(1);
+}
+
+const clickZoomIn = function() {
+    map.zoomIn(1, { animate: true });
+    setZoomLabel();
+};
+
+const clickZoomOut = function() {
+    map.zoomOut(1, { animate: true });
+    setZoomLabel();
+};
+
 export default class MapPanel extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             open: true
         };
+        this.toggleMapPanel = this.toggleMapPanel.bind(this);
+    }
+
+    componentDidMount() {
+        // search.init();
+        // initGeolocator();
+        // bookmarks.init();
+        // setZoomLabel();
+        //
+        // currentLocation = map.getCenter();
+        // search.setCurrentLatLng(currentLocation);
+        // search.reverseGeocode(currentLocation);
+    }
+
+    toggleMapPanel() {
+        this.setState({ open: !this.state.open });
     }
 
     render() {
         return (
             <div>
                 {/*Toggle map panel to show it*/}
-                <Button onClick={ ()=> this.setState({ open: !this.state.open })} className='map-panel-button-show'>
+                <Button onClick={this.toggleMapPanel} className='map-panel-button-show'>
                     <Icon type={'bt-caret-down'} />
                 </Button>
 
@@ -31,10 +76,10 @@ export default class MapPanel extends React.Component {
                         {/*Zoom buttons*/}
                         <ButtonGroup id="buttons-plusminus">
                             <OverlayTrigger placement='bottom' overlay={<Tooltip id='tooltip'>{'Zoom in'}</Tooltip>}>
-                                <Button> <Icon type={'bt-plus'} /> </Button>
+                                <Button onClick={clickZoomIn}> <Icon type={'bt-plus'} /> </Button>
                             </OverlayTrigger>
                             <OverlayTrigger placement='bottom' overlay={<Tooltip id='tooltip'>{'Zoom out'}</Tooltip>}>
-                                <Button> <Icon type={'bt-minus'} /> </Button>
+                                <Button onClick={clickZoomOut}> <Icon type={'bt-minus'} /> </Button>
                             </OverlayTrigger>
                         </ButtonGroup>
 
@@ -66,7 +111,7 @@ export default class MapPanel extends React.Component {
                         {/*Toggle map panel to show it*/}
                         <ButtonGroup>
                             <OverlayTrigger placement='bottom' overlay={<Tooltip id='tooltip'>{'Toggle map toolbar'}</Tooltip>}>
-                                <Button onClick={ ()=> this.setState({ open: !this.state.open })}> <Icon type={'bt-caret-up'} /> </Button>
+                                <Button onClick={this.toggleMapPanel}> <Icon type={'bt-caret-up'} /> </Button>
                             </OverlayTrigger>
                         </ButtonGroup>
                     </div>
