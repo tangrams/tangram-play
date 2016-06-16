dbdeb493d511e086da9e1b94cca8db519347e377
Lou Huang <lou@louhuang.com>
2016-06-16 15:33:21 -0400

    Minimum font size should not be a magic number

--- src/js/editor/editor.js
+++ src/js/editor/editor.js
@@ -163,7 +163,7 @@ function createRulersOption (indentSize = INDENT_UNIT, amount = 10) {
 }
 
 /**
- * Change font size in editor.
+ * Change font size in editor. Increments or decrements by 1px.
  * Font size will never go below 8px.
  *
  * @param {CodeMirror} cm - instance of CodeMirror editor.
@@ -171,10 +171,11 @@ function createRulersOption (indentSize = INDENT_UNIT, amount = 10) {
  *              goes down.
  */
 function changeFontSize (cm, increase) {
+    const MINIMUM_FONT_SIZE = 8;
     const el = cm.getWrapperElement();
     const fontSize = window.getComputedStyle(el).getPropertyValue('font-size');
     const adjustment = increase ? 1 : -1;
-    const newSize = Math.max(window.parseInt(fontSize, 10) + adjustment, 8);
+    const newSize = Math.max(window.parseInt(fontSize, 10) + adjustment, MINIMUM_FONT_SIZE);
     el.style.fontSize = newSize.toString() + 'px';
     cm.refresh();
 }
