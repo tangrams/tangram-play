2d5e8b9638b3be1f8b0d4a051bdb2674e6a88e6b
Lou Huang <lou@louhuang.com>
2016-06-30 12:58:59 -0400

    Add cache-scenes script and README.

---
+++ meta/example-scenes/cache-scenes.js
@@ -0,0 +1,46 @@
+/**
+ * A node script for fetching and saving example scene files for Tangram Play
+ * locally. For more information, see README.md in this directory.
+ */
+'use strict';
+
+// Keep dependencies low, only use Node.js native modules
+const path = require('path');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const IMAGE_SOURCES = require('./sources');
+const WRITE_PATH = '../../data/scenes';
+
+for (let name in IMAGE_SOURCES) {
+    const url = IMAGE_SOURCES[name].scene;
+
+    // Not all scenes have urls to cache; move on to the next if that's the case
+    if (!url) {
+        continue;
+    }
+
+    const destination = path.resolve(__dirname, WRITE_PATH, name + '.yaml');
+
+    // Depending on the URL's protocol, Node either needs the `http` or `https`
+    // module.
+    const protocol = url.startsWith('https') ? https : http;
+
+    protocol.get(url, (response) => {
+        // Continuously update stream with data
+        let body = '';
+        response.on('data', function (data) {
+            body += data;
+        });
+        response.on('end', function () {
+            // Data reception is done, save it to destination
+            fs.writeFile(destination, body, (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(`${destination} - done.`);
+            });
+        });
+    });
+}
--- meta/example-scenes/cache-thumbnails.js
+++ meta/example-scenes/cache-thumbnails.js
@@ -1,24 +1,9 @@
 /**
  * A node script for making the thumbnails for scene files displayed in
- * Tangram Play's "Open an example" modal. The images are static resources
- * that will be stored directly in this repository, and deployed along with
- * Tangram Play, reducing its dependency on GitHub or RawGit to operate. This
- * is designed to run only when needed, so it is not part of the build or
- * deploy process.
+ * Tangram Play's "Open an example" modal. For more information, see
+ * README.md in this directory.
  *
- * This script provides an object whose keys are the filename to save to and
- * values that are an external URL where that image resides. Each URL is
- * downloaded, resized, and cropped to the dimensions that Tangram Play expects,
- * optimized for file size and rendering, and saved to the /data/scenes/thumbnails
- * path in this repository. When this script is run, all valid URLs that match
- * existing thumbnail images will replace them. If you remove a URL here, you
- * will also have to remove the thumbnail yourself, if you need to. URLs that
- * are not valid for any reason (e.g. 404, 500 errors) are skipped with a warning.
- *
- * You may edit this script to provide new image URL sources, or update image
- * dimensions. When images are updated or created, commit the changes to the
- * repository. Be sure to update Tangram Play's `examples.json` so that the
- * correct images appear in the "Open an example" menu.
+ * You may edit this script to update image dimensions.
  *
  * To run this script, you will need a native installation of ImageMagick.
  * On Mac OS X, you can use Homebrew:
