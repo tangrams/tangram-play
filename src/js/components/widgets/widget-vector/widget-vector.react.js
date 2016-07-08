04328510af8081b9a43d070320a06b4a15e80802
Irene Alvarado <ire.alvarado@gmail.com>
2016-07-08 11:47:03 -0400

    Adding a prototype vector widget with Three.js that displays a line and a 3D axis

--- src/js/components/widgets/widget-vector/widget-vector.react.js
+++ src/js/components/widgets/widget-vector/widget-vector.react.js
@@ -3,6 +3,7 @@ import Modal from 'react-bootstrap/lib/Modal';
 import Button from 'react-bootstrap/lib/Button';
 import DraggableModal from '../../draggable-modal.react.js';
 import Icon from '../../icon.react';
+import { setNodeValue } from '../../../editor/editor';
 
 import THREE from 'three';
 import TrackballControls from './TrackBallControls';
@@ -38,97 +39,118 @@ export default class WidgetVector extends React.Component {
     }
 
     componentDidUpdate () {
-        if(window.WebGLRenderingContext) {
+        if (window.WebGLRenderingContext) {
             renderer = new THREE.WebGLRenderer({ antialias: true });
-            console.log(renderer.domElement);
-            console.log(this.refs.mytest);
-            this.refs.mytest.appendChild( renderer.domElement );
+            this.refs.mytest.appendChild(renderer.domElement);
 
-            renderer.setSize( 300, 300 );
-	        renderer.setClearColor( 0xeeeeee, 1.0 );
+            renderer.setSize(300, 300);
+            renderer.setClearColor(0xeeeeee, 1.0);
 
-	        scene = new THREE.Scene();
+            scene = new THREE.Scene();
 
             let meshMaterial = new THREE.MeshBasicMaterial({ color: 0xFF00FF, wireframe: true });
 
-        	var cube = new THREE.Mesh( new THREE.CubeGeometry( 5, 5, 5 ), meshMaterial );
-        	cube.position.set( 25, 25, 25 );
-        	scene.add( cube );
+            var cube = new THREE.Mesh(new THREE.CubeGeometry(5, 5, 5), meshMaterial);
+            cube.position.set(25, 25, 25);
+            scene.add(cube);
 
             // Add axes
-        	let axes = this.buildAxes( 1000 );
-        	scene.add( axes );
+            let axes = this.buildAxes(1000);
+            scene.add(axes);
 
-            camera = new THREE.PerspectiveCamera( 45, 300/300, 1, 10000 );
-        	camera.position.set( 30, 50, 120 );
-        	camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
+            camera = new THREE.PerspectiveCamera(45, 300 / 300, 1, 10000);
+            camera.position.set(30, 50, 120);
+            camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-            controls = new TrackballControls( camera, renderer.domElement );
+            controls = new TrackballControls(camera, renderer.domElement);
             controls.rotateSpeed = 1.0;
-        	controls.zoomSpeed = 0.2;
-        	controls.panSpeed = 0.8;
+            controls.zoomSpeed = 0.2;
+            controls.panSpeed = 0.8;
 
-        	controls.noZoom = false;
-        	controls.noPan = false;
+            controls.noZoom = false;
+            controls.noPan = false;
 
-        	controls.staticMoving = true;
-        	controls.dynamicDampingFactor = 0.3;
+            controls.staticMoving = true;
+            controls.dynamicDampingFactor = 0.3;
+
+            let lineToMove = this.buildLine();
+            scene.add(lineToMove);
 
             this.animate();
-
         }
     }
 
     animate () {
-        requestAnimationFrame( this.animate );
+        requestAnimationFrame(this.animate);
         controls.update();
-		renderer.render( scene, camera );
+        renderer.render(scene, camera);
+    }
+
+    buildLine () {
+        let mat = new THREE.LineBasicMaterial({
+            linewidth: 5,
+            color: 0xFF99FF,
+            linecap: 'butt',
+            linejoin: 'bevel'
+        });
+
+        let geom = new THREE.Geometry();
+        let line3 = new THREE.Line3(new THREE.Vector3(0, 0, 0), new THREE.Vector3(80, 80, 80));
+
+        // geom.vertices.push(new THREE.Vector3(0, 0, 0));
+        // geom.vertices.push(new THREE.Vector3(80, 80, 80));
+        geom.vertices.push(line3.start);
+        geom.vertices.push(line3.end);
+
+        let line = new THREE.Line(geom, mat);
+
+        return line;
     }
 
     buildAxes (length) {
         let axes = new THREE.Object3D();
 
-		axes.add( this.buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( length, 0, 0 ), 0xFF0000, false ) ); // +X
-		axes.add( this.buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -length, 0, 0 ), 0xFF0000, true) ); // -X
-		axes.add( this.buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, length, 0 ), 0x00FF00, false ) ); // +Y
-		axes.add( this.buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -length, 0 ), 0x00FF00, true ) ); // -Y
-		axes.add( this.buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, length ), 0x0000FF, false ) ); // +Z
-		axes.add( this.buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -length ), 0x0000FF, true ) ); // -Z
+        axes.add(this.buildAxis(new THREE.Vector3(0, 0, 0), new THREE.Vector3(length, 0, 0), 0xFF0000, false)); // +X
+        axes.add(this.buildAxis(new THREE.Vector3(0, 0, 0), new THREE.Vector3(-length, 0, 0), 0xFF0000, true)); // -X
+        axes.add(this.buildAxis(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, length, 0), 0x00FF00, false)); // +Y
+        axes.add(this.buildAxis(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, -length, 0), 0x00FF00, true)); // -Y
+        axes.add(this.buildAxis(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, length), 0x0000FF, false)); // +Z
+        axes.add(this.buildAxis(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, -length), 0x0000FF, true)); // -Z
 
-		return axes;
+        return axes;
     }
 
     buildAxis (src, dst, colorHex, dashed) {
-		let geom = new THREE.Geometry();
-		let mat;
+        let geom = new THREE.Geometry();
+        let mat;
 
-		if (dashed) {
-			mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
-		}
+        if (dashed) {
+            mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
+        }
         else {
-			mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
-		}
+            mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
+        }
 
-		geom.vertices.push( src.clone() );
-		geom.vertices.push( dst.clone() );
-		geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+        geom.vertices.push(src.clone());
+        geom.vertices.push(dst.clone());
+        geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
 
-		// let axis = new THREE.Line( geom, mat, THREE.LinePieces );
+        // let axis = new THREE.Line(geom, mat, THREE.LinePieces);
         // http://stackoverflow.com/questions/32915473/three-js-r72-no-longer-supports-three-linepieces-how-to-merge-multiple-disconne
-        // let axis = new THREE.Line( geom, mat, THREE.LineSegments );
+        // let axis = new THREE.Line(geom, mat, THREE.LineSegments);
 
-        // let test = new THREE.LineSegments( geom, mat );
-        let axis = new THREE.Line( geom, mat );
+        // let test = new THREE.LineSegments(geom, mat);
+        let axis = new THREE.Line(geom, mat);
 
-		return axis;
-	}
+        return axis;
+    }
 
     /**
      * Open or close the color picker widget
      */
     handleClick () {
         this.setState({ displayPicker: !this.state.displayPicker });
-        console.log("click");
+        console.log('click');
     }
 
     /* SHARED METHODS FOR ALL WIDGETS? */
@@ -176,7 +198,7 @@ export default class WidgetVector extends React.Component {
                     <div ref='mytest'></div>
                 </Modal>
             </div>
-        );
+       );
     }
 }
 
