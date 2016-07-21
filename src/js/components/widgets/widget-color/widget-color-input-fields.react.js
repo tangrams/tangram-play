81430760757a762580c5256b8637f7cafcb4b2bf
Irene Alvarado <ire.alvarado@gmail.com>
2016-07-21 10:16:52 -0400

    Improving the way color value is passed around in the color wiget

--- src/js/components/widgets/widget-color/widget-color-input-fields.react.js
+++ src/js/components/widgets/widget-color/widget-color-input-fields.react.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import shallowCompare from 'react-addons-shallow-compare';
 import { EditableInput } from 'react-color/lib/components/common';
-import color from 'react-color/lib/helpers/color';
 
 export default class WidgetColorInputFields extends React.Component {
     constructor (props) {
@@ -16,52 +15,53 @@ export default class WidgetColorInputFields extends React.Component {
     }
 
     onChange (data) {
-        console.log("within color field");
-        console.log(data);
-        if (data.hex && color.isValidHex(data.hex)) {
+        let color = this.props.color.getRgba();
+        if (data.hex) {
             this.props.onChange({
-                hex: data.hex,
-                source: 'hex',
+                hex: data.hex
             });
         }
-        else if (data.r || data.g || data.b) {
-            this.props.onChange({
-                r: data.r || this.props.rgb.r,
-                g: data.g || this.props.rgb.g,
-                b: data.b || this.props.rgb.b,
-                a: this.props.rgb.a,
-                source: 'rgb',
-            });
-        }
-        else if (data.a) {
-            if (data.a < 0) {
-                data.a = 0;
-            }
-            else if (data.a > 100) {
-                data.a = 100;
-            }
+        else if (data.r || data.g || data.b || data.a) {
+            let a = parseInt(data.a);
 
-            data.a = data.a / 100;
+            if (a === 0) {
+                this.props.onChange({
+                    r: data.r || color.r,
+                    g: data.g || color.g,
+                    b: data.b || color.b,
+                    a: 0.0
+                });
+            }
+            else {
+                this.props.onChange({
+                    r: data.r || color.r,
+                    g: data.g || color.g,
+                    b: data.b || color.b,
+                    a: (a / 100) || color.a
+                });
+            }
         }
     }
 
     render () {
+        let color = this.props.color.getRgba();
+        let hex = this.props.color.getHexString();
         return (
             <div className='fields'>
                 <div className='double'>
-                    <EditableInput className='input' label='hex' value={ this.props.color.getHexString() } onChange={ this.onChange }/>
+                    <EditableInput className='input' label='hex' value={ hex } onChange={ this.onChange }/>
                 </div>
                 <div className='single'>
-                    <EditableInput className='input' label='r' value={ this.props.color.getRgba().r } onChange={ this.onChange }/>
+                    <EditableInput className='input' label='r' value={ color.r } onChange={ this.onChange }/>
                 </div>
                 <div className='single'>
-                    <EditableInput className='input' label='g' value={ this.props.color.getRgba().g } onChange={ this.onChange } />
+                    <EditableInput className='input' label='g' value={ color.g } onChange={ this.onChange } />
                 </div>
                 <div className='single'>
-                    <EditableInput className='input' label='b' value={ this.props.color.getRgba().b } onChange={ this.onChange } />
+                    <EditableInput className='input' label='b' value={ color.b } onChange={ this.onChange } />
                 </div>
                 <div className='alpha3'>
-                    <EditableInput is='input' label='a' value={ Math.round(this.props.color.getRgba().a * 100) } onChange={ this.onChange } />
+                    <EditableInput is='input' label='a' value={ Math.round(color.a * 100) } onChange={ this.onChange } />
                 </div>
             </div>
         );
