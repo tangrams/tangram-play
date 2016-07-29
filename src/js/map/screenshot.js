a3fc3d01446202b975e58191ff8a23f126a67087
Lou Huang <lou@louhuang.com>
2016-07-29 13:01:17 -0700

    Adds x/y/z map position to filename slug, resolves #474

--- src/js/map/screenshot.js
+++ src/js/map/screenshot.js
@@ -1,5 +1,5 @@
 import { saveAs } from '../vendor/FileSaver.min.js';
-import { tangramScene } from './map';
+import { map, tangramScene } from './map';
 
 /**
  * Uses Tangram's native screenshot functionality to download an image.
@@ -9,7 +9,7 @@ import { tangramScene } from './map';
  */
 export function takeScreenshot () {
     tangramScene.screenshot().then(function (result) {
-        const slug = createFilename();
+        const slug = createFilenameSlug(map);
 
         // uses FileSaver.js: https://github.com/eligrey/FileSaver.js/
         saveAs(result.blob, `tangram-${slug}.png`);
@@ -35,10 +35,28 @@ export function getScreenshotData () {
  * Utility function to generate a filename slug from current date and
  * current map view properties.
  *
- * @returns {string}
+ * @param {Leaflet} map - we'll get the map view data from this.
+ * @returns {string} - see format in comments, inline
  */
-function createFilename () {
-    let slug = new Date().toString();
+function createFilenameSlug (map) {
+    const date = new Date();
 
-    return slug;
+    // Get string values for each portion of the date, left-padding the
+    // trailing 0 if necessary.
+    const year = date.getFullYear().toString();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2); // 0-indexed, add 1
+    const day = ('0' + date.getDate()).slice(-2);
+    const hour = ('0' + date.getHours()).slice(-2); // 24-hour clock.
+    const minute = ('0' + date.getMinutes()).slice(-2);
+    const second = ('0' + date.getSeconds()).slice(-2);
+
+    // Get map view
+    const center = map.getCenter();
+    const zoom = map.getZoom().toFixed(4).toString();
+    const lat = center.lat.toFixed(4).toString();
+    const lng = center.lng.toFixed(4).toString();
+
+    // String format, eg.
+    // 2016-07-29 12.59.16 z8.7067.x76.2119.y-60.8138
+    return `${year}-${month}-${day} ${hour}.${minute}.${second} z${zoom}.x${lat}.y${lng}`;
 }
