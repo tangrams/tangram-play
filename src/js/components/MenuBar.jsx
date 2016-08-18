f17d77fd812d9e813d4d55e37134cb7f3fd8fcff
Lou Huang <lou@louhuang.com>
2016-08-18 17:37:23 -0400

    Reformat the MenuBar a little bit

--- src/js/components/MenuBar.jsx
+++ src/js/components/MenuBar.jsx
@@ -78,78 +78,6 @@ export default class MenuBar extends React.Component {
         };
     }
 
-    /**
-     * Official React lifecycle method
-     * Called every time state or props are changed
-     */
-    render () {
-        return (
-            <Navbar inverse className='menu-bar'>
-                {/* The brand section */}
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <span className='brand'>Tangram Play<span className='brand-tag'>BETA</span></span>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-
-                <Navbar.Collapse>
-                    {/* Left menu section */}
-                    <Nav pullLeft>
-                        {/* New button*/}
-                        <OverlayTrigger rootClose placement='bottom' overlay={<Tooltip id='tooltip'>{'New scene'}</Tooltip>}>
-                            <NavItem eventKey={'new'} onClick={_clickNew} href='#'><Icon type={'bt-file'} />New</NavItem>
-                        </OverlayTrigger>
-
-                        {/* Open dropdown */}
-                        <OverlayTrigger rootClose placement='bottom' overlay={<Tooltip id='tooltip'>{'Open scene'}</Tooltip>}>
-                            <NavDropdown title={<span><Icon type={'bt-upload'} />Open</span>} id='open-dropdown'>
-                                <MenuItem onClick={_clickOpenFile}><Icon type={'bt-folder'} />Open a file</MenuItem>
-                                <MenuItem onClick={_clickOpenGist}><Icon type={'bt-code'} />Open a saved Gist</MenuItem>
-                                <MenuItem onClick={_clickOpenURL}><Icon type={'bt-link'} />Open from URL</MenuItem>
-                                <MenuItem onClick={_clickOpenExample}><Icon type={'bt-map'} />Choose example</MenuItem>
-                            </NavDropdown>
-                        </OverlayTrigger>
-
-                        {/* Save dropdown */}
-                        <OverlayTrigger rootClose placement='bottom' overlay={<Tooltip id='tooltip'>{'Save scene'}</Tooltip>}>
-                            <NavDropdown title={<span><Icon type={'bt-download'} />Save</span>} id='save-dropdown'>
-                                <MenuItem onClick={_clickSaveFile}><Icon type={'bt-folder'} />Save to file</MenuItem>
-                                <MenuItem onClick={_clickSaveGist}><Icon type={'bt-code'} />Save to Gist</MenuItem>
-                                <MenuItem onClick={_clickSaveCamera}><Icon type={'bt-camera'} />Take a screenshot</MenuItem>
-                            </NavDropdown>
-                        </OverlayTrigger>
-                    </Nav>
-
-                    {/* Right menu section */}
-                    <Nav pullRight>
-                        {/* Introspection button */}
-                        <OverlayTrigger rootClose placement='bottom' overlay={<Tooltip id='tooltip'>{'Toggle inspect mode'}</Tooltip>}>
-                            <NavItem eventKey={'new'} onClick={this._clickInspect.bind(this)} href='#' active={this.state.inspectActive}><Icon type={'bt-magic'} />Inspect</NavItem>
-                        </OverlayTrigger>
-
-                        {/* Fullscreen button */}
-                        <OverlayTrigger rootClose placement='bottom' overlay={<Tooltip id='tooltip'>{'View fullscreen'}</Tooltip>}>
-                            <NavItem eventKey={'new'} onClick={this._clickFullscreen.bind(this)} href='#' active={this.state.fullscreenActive}><Icon type={'bt-maximize'} />Fullscreen</NavItem>
-                        </OverlayTrigger>
-
-                        {/* Help dropdown */}
-                        <OverlayTrigger rootClose placement='bottom' overlay={<Tooltip id='tooltip'>{'Documentation and help'}</Tooltip>}>
-                            <NavDropdown title={<span><Icon type={'bt-question-circle'} />Help</span>} id='help-dropdown'>
-                                <MenuItem onClick={_clickAbout}><Icon type={'bt-info-circle'} />About</MenuItem>
-                                <MenuItem href={documentationLink} target='_blank'><Icon type={'bt-book'} />Documentation</MenuItem>
-                                <MenuItem href={feedbackLink} target='_blank'><Icon type={'bt-comments'} />Feedback</MenuItem>
-                            </NavDropdown>
-                        </OverlayTrigger>
-
-                        {/* Sign in */}
-                        <SignInButton />
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
-
     _clickFullscreen () {
         this.setState({ fullscreenActive: !this.state.fullscreenActive });
         toggleFullscreen();
@@ -165,5 +93,147 @@ export default class MenuBar extends React.Component {
             this.setState({ inspectActive: true });
             setGlobalIntrospection(true);
         }
+    }
+
+    /**
+     * Official React lifecycle method
+     * Called every time state or props are changed
+     */
+    render () {
+        return (
+            <Navbar inverse className='menu-bar'>
+                {/* The brand section */}
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <span className='brand'>
+                            Tangram Play
+                            <span className='brand-tag'>BETA</span>
+                        </span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+
+                <Navbar.Collapse>
+                    {/* Left menu section */}
+                    <Nav pullLeft>
+                        {/* New button*/}
+                        <OverlayTrigger
+                            rootClose
+                            placement='bottom'
+                            overlay={<Tooltip id='tooltip'>{'New scene'}</Tooltip>}
+                        >
+                            <NavItem eventKey={'new'} onClick={_clickNew}>
+                                <Icon type={'bt-file'} />New
+                            </NavItem>
+                        </OverlayTrigger>
+
+                        {/* Open dropdown */}
+                        <OverlayTrigger
+                            rootClose
+                            placement='bottom'
+                            overlay={<Tooltip id='tooltip'>{'Open scene'}</Tooltip>}
+                        >
+                            <NavDropdown
+                                title={<span><Icon type={'bt-upload'} />Open</span>}
+                                id='open-dropdown'
+                            >
+                                <MenuItem onClick={_clickOpenFile}>
+                                    <Icon type={'bt-folder'} />Open a file
+                                </MenuItem>
+                                <MenuItem onClick={_clickOpenGist}>
+                                    <Icon type={'bt-code'} />Open a saved Gist
+                                </MenuItem>
+                                <MenuItem onClick={_clickOpenURL}>
+                                    <Icon type={'bt-link'} />Open from URL
+                                </MenuItem>
+                                <MenuItem onClick={_clickOpenExample}>
+                                    <Icon type={'bt-map'} />Choose example
+                                </MenuItem>
+                            </NavDropdown>
+                        </OverlayTrigger>
+
+                        {/* Save dropdown */}
+                        <OverlayTrigger
+                            rootClose
+                            placement='bottom'
+                            overlay={<Tooltip id='tooltip'>{'Save scene'}</Tooltip>}
+                        >
+                            <NavDropdown
+                                title={<span><Icon type={'bt-download'} />Save</span>}
+                                id='save-dropdown'
+                            >
+                                <MenuItem onClick={_clickSaveFile}>
+                                    <Icon type={'bt-folder'} />Save to file
+                                </MenuItem>
+                                <MenuItem onClick={_clickSaveGist}>
+                                    <Icon type={'bt-code'} />Save to Gist
+                                </MenuItem>
+                                <MenuItem onClick={_clickSaveCamera}>
+                                    <Icon type={'bt-camera'} />Take a screenshot
+                                </MenuItem>
+                            </NavDropdown>
+                        </OverlayTrigger>
+                    </Nav>
+
+                    {/* Right menu section */}
+                    <Nav pullRight>
+                        {/* Introspection button */}
+                        <OverlayTrigger
+                            rootClose
+                            placement='bottom'
+                            overlay={<Tooltip id='tooltip'>{'Toggle inspect mode'}</Tooltip>}
+                        >
+                            <NavItem
+                                eventKey={'new'}
+                                onClick={this._clickInspect.bind(this)}
+                                active={this.state.inspectActive}
+                            >
+                                <Icon type={'bt-magic'} />Inspect
+                            </NavItem>
+                        </OverlayTrigger>
+
+                        {/* Fullscreen button */}
+                        <OverlayTrigger
+                            rootClose
+                            placement='bottom'
+                            overlay={<Tooltip id='tooltip'>{'View fullscreen'}</Tooltip>}
+                        >
+                            <NavItem
+                                eventKey={'new'}
+                                onClick={this._clickFullscreen.bind(this)}
+                                active={this.state.fullscreenActive}
+                            >
+                                <Icon type={'bt-maximize'} />Fullscreen
+                            </NavItem>
+                        </OverlayTrigger>
+
+                        {/* Help dropdown */}
+                        <OverlayTrigger
+                            rootClose
+                            placement='bottom'
+                            overlay={<Tooltip id='tooltip'>{'Documentation and help'}</Tooltip>}
+                        >
+                            <NavDropdown
+                                title={<span><Icon type={'bt-question-circle'} />Help</span>}
+                                id='help-dropdown'
+                            >
+                                <MenuItem onClick={_clickAbout}>
+                                    <Icon type={'bt-info-circle'} />About
+                                </MenuItem>
+                                <MenuItem href={documentationLink} target='_blank'>
+                                    <Icon type={'bt-book'} />Documentation
+                                </MenuItem>
+                                <MenuItem href={feedbackLink} target='_blank'>
+                                    <Icon type={'bt-comments'} />Feedback
+                                </MenuItem>
+                            </NavDropdown>
+                        </OverlayTrigger>
+
+                        {/* Sign in */}
+                        <SignInButton />
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
     }
 }
