bf30f08645f1c415764159b87b04f5a86ff80128
Irene Alvarado <ire.alvarado@gmail.com>
2016-08-24 17:54:32 -0400

    Fixing typo in Icon component

--- src/js/components/RefreshButton.jsx
+++ src/js/components/RefreshButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
-import Icon from './icon';
+import Icon from './Icon';
 
 import { reloadOriginalScene } from '../tangram-play';
 
