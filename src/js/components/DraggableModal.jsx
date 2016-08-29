8011799dd5567690c39e9ae8aae8a49394d37d80
Lou Huang <lou@louhuang.com>
2016-08-29 19:24:56 -0400

    Reformat DraggableModal. Add a note about where it comes from.

--- src/js/components/DraggableModal.jsx
+++ src/js/components/DraggableModal.jsx
@@ -5,10 +5,22 @@ import ModalDialog from 'react-bootstrap/lib/ModalDialog';
 /**
  * Represents a draggable container for a modal
  * Important: attaches to a div with a class 'drag'
+ *
+ * This is a workaround for Draggable not working directly with react-bootstrap
+ * Modal components. See here. https://github.com/mzabriskie/react-draggable/issues/56
  */
-export default class DraggableModal extends React.Component {
+export default class DraggableModal extends React.PureComponent {
     render () {
-        return <Draggable bounds='#draggable-container' defaultPosition={{x: this.props.x, y: this.props.y}} zIndex={1800} handle='.drag'><ModalDialog {...this.props} /></Draggable>;
+        return (
+            <Draggable
+                bounds='#draggable-container'
+                defaultPosition={{x: this.props.x, y: this.props.y}}
+                zIndex={1800}
+                handle='.drag'
+            >
+                <ModalDialog {...this.props} />
+            </Draggable>
+        );
     }
 }
 
