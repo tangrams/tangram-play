4e844a04fc758093564b35e4ac0ce0866da128fa
Lou Huang <lou@louhuang.com>
2016-08-29 10:57:55 -0400

    App containers never need to update

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -14,6 +14,10 @@ export default class App extends React.Component {
         initTangramPlay();
     }
 
+    shouldComponentUpdate () {
+        return false;
+    }
+
     render () {
         return (
             <div>
