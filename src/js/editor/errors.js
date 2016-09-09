63aaaf6d87a556a174a381101d698180351bfed2
Lou Huang <lou@louhuang.com>
2016-09-09 18:31:18 -0400

    Refactor error manager

--- src/js/editor/errors.js
+++ src/js/editor/errors.js
@@ -5,6 +5,49 @@ import EventEmitter from '../components/event-emitter';
 const lineWidgets = [];
 const blockErrors = new Set();
 
+/**
+ * Creates DOM element to be injected into the editor and display as an error.
+ *
+ * @param {string} type - either 'error' or 'warning'
+ * @param {string} message - the string to display in the error
+ */
+function createErrorLineElement(type, message) {
+    let iconTypeClass;
+    if (type === 'error') {
+        iconTypeClass = 'btm bt-exclamation-triangle error-icon';
+    } else if (type === 'warning') {
+        iconTypeClass = 'btm bt-exclamation-circle warning-icon';
+    }
+
+    const node = document.createElement('div');
+    node.className = type;
+
+    const icon = document.createElement('span');
+    icon.className = iconTypeClass;
+
+    node.appendChild(icon);
+    node.appendChild(document.createTextNode(message));
+
+    return node;
+}
+
+/**
+ * Creates CodeMirror line widget to display an error below a line.
+ *
+ * @param {string} type - either 'error' or 'warning'
+ * @param {Number} line - the line number to display under
+ * @param {string} message - the string to display in the error
+ */
+function createLineWidget(type, line, message) {
+    const node = createErrorLineElement(type, message);
+    const lineWidget = editor.addLineWidget(line, node, {
+        coverGutter: false,
+        noHScroll: true,
+    });
+
+    lineWidgets.push(lineWidget);
+}
+
 function clearAllErrors() {
     for (let i = 0; i < lineWidgets.length; i++) {
         editor.removeLineWidget(lineWidgets[i]);
@@ -13,65 +56,77 @@ function clearAllErrors() {
     blockErrors.clear();
 }
 
-function addError(args) {
-    if (args.type !== undefined) {
-        const msg = document.createElement('div');
-        const icon = msg.appendChild(document.createElement('span'));
-        icon.className = 'btm bt-exclamation-triangle error-icon';
-        msg.appendChild(document.createTextNode(args.error.reason));
-        msg.className = 'error';
-        lineWidgets.push(editor.addLineWidget(args.error.mark.line, msg, { coverGutter: false, noHScroll: true }));
+/**
+ * Handles error object returned by Tangram's error event.
+ *
+ * @param {Object} errorObj - error object from Tangram. Its signature will
+ *          vary depending on the `errorObj.type` property.
+ */
+function addError(errorObj) {
+    switch (errorObj.type) {
+        case 'yaml': {
+            const line = errorObj.error.mark.line;
+            const message = errorObj.error.reason;
+            createLineWidget('error', line, message);
+            break;
+        }
+        // case 'scene':
+        case 'scene_import':
+            // errorObj contains these properties:
+            //      `message` - handy error from Tangram
+            //      `url` - the url that could not be loaded
+            // Unfortunately we do not have a line number on which the error exists
+            console.log(errorObj.message, errorObj.url);
+            break;
+        // Default case handles unknown error types or undefined types, which
+        // can happen if Tangram Play itself (and not Tangram) throws an error
+        // when Tangram is executing a Promise and catches a Tangram Play error.
+        default:
+            console.log('unknown error', errorObj);
+            break;
     }
 }
 
-function addWarning(args) {
-    if (args.type === 'styles') {
-        // Only show first error, cascading errors can be confusing
-        const errors = args.shader_errors.slice(0, 1);
+function addWarning(errorObj) {
+    switch (errorObj.type) {
+        case 'styles': {
+            // Only show first error, cascading errors can be confusing
+            const errors = errorObj.shader_errors.slice(0, 1);
 
-        for (let i = 0; i < errors.length; i++) {
-            const style = errors[i].block.scope;
+            for (let i = 0; i < errors.length; i++) {
+                const style = errors[i].block.scope;
 
-            // Skip generic errors not originating in style-sheet
-            if (style === 'ShaderProgram') {
-                continue; // eslint-disable-line no-continue
+                // Skip generic errors not originating in style-sheet
+                if (style === 'ShaderProgram') {
+                    continue; // eslint-disable-line no-continue
+                }
+
+                const block = errors[i].block;
+
+                // De-dupe errors per block
+                if (blockErrors.has(JSON.stringify(block))) {
+                    continue; // eslint-disable-line no-continue
+                }
+
+                const address = `styles:${style}:shaders:blocks:${block.name}`;
+                const node = getNodesForAddress(address);
+
+                if (node) {
+                    const line = node.range.from.line + 1 + block.line;
+                    const message = errors[i].message;
+                    createLineWidget('warning', line, message);
+                    blockErrors.add(JSON.stringify(block)); // track unique errors
+                } else {
+                    // TODO: Report a general level error
+                    console.log('Node', address, 'was not found');
+                }
             }
 
-            const block = errors[i].block;
-
-            // De-dupe errors per block
-            if (blockErrors.has(JSON.stringify(block))) {
-                continue; // eslint-disable-line no-continue
-            }
-
-            const address = `styles:${style}:shaders:blocks:${block.name}`;
-            const node = getNodesForAddress(address);
-
-            if (node) {
-                const nLine = node.range.from.line + 1 + block.line;
-
-                const msg = document.createElement('div');
-                const icon = msg.appendChild(document.createElement('span'));
-                icon.className = 'btm bt-exclamation-circle warning-icon';
-                msg.appendChild(document.createTextNode(errors[i].message));
-                msg.className = 'warning';
-                lineWidgets.push(editor.addLineWidget(nLine, msg, { coverGutter: false, noHScroll: true }));
-                blockErrors.add(JSON.stringify(block)); // track unique errors
-            } else {
-                console.log('Node', address, 'was not found');
-            }
+            break;
         }
-    } else if (args.type === 'duplicate') {
-        for (const node of args.nodes) {
-            console.log(node);
-            const nLine = node.widget.range.to.line + 1;
-            const msg = document.createElement('div');
-            const icon = msg.appendChild(document.createElement('span'));
-            icon.className = 'btm bt-exclamation-circle warning-icon';
-            msg.appendChild(document.createTextNode(`Duplicate key ${node.key} (${node.address})`));
-            msg.className = 'warning';
-            lineWidgets.push(editor.addLineWidget(nLine, msg, { coverGutter: false, noHScroll: true }));
-        }
+        // Handle unknown warning types.
+        default:
+            break;
     }
 }
 
@@ -81,13 +136,14 @@ export function initErrorsManager() {
     });
 
     // Subscribe to error events from Tangram
+    // See documentation: https://mapzen.com/documentation/tangram/Javascript-API/#error-and-warning
     EventEmitter.subscribe('tangram:sceneinit', () => {
         tangramLayer.scene.subscribe({
-            error: (args) => {
-                addError(args);
+            error: (event) => {
+                addError(event);
             },
-            warning: (args) => {
-                addWarning(args);
+            warning: (event) => {
+                addWarning(event);
             },
         });
     });
