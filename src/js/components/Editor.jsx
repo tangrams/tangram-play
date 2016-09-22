797d8d9436d5cf3e397373649684531e96d4ff9f
Lou Huang <lou@louhuang.com>
2016-09-22 16:48:20 -0400

    Put docs panel availability behind the admin flag

--- src/js/components/Editor.jsx
+++ src/js/components/Editor.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-// import DocsPanel from './DocsPanel';
+import { connect } from 'react-redux';
+import DocsPanel from './DocsPanel';
 import { initEditor } from '../editor/editor';
 import Divider from './Divider';
 
-export default class Editor extends React.Component {
+let docsHasInitAlready = false;
+
+class Editor extends React.PureComponent {
     componentDidMount() {
         // instantiate CodeMirror with the editor container element's
         // DOM node reference
         initEditor(this.editorEl);
+    }
 
-        // DocsPanel is temporarily disabled.
-        // this.docsPanel.init();
+    componentDidUpdate() {
+        // DocsPanel is only available behind an admin flag.
+        // Update after props have determined sign-in, and init docsPanel.
+        // Only do this once.
+        if (!docsHasInitAlready && this.props.admin) {
+            this.docsPanel.init();
+            docsHasInitAlready = true;
+        }
     }
 
     render() {
@@ -19,8 +29,31 @@ export default class Editor extends React.Component {
             <div className="editor-container" id="content">
                 <Divider />
                 <div className="editor" id="editor" ref={(ref) => { this.editorEl = ref; }} />
-                {/* <DocsPanel ref={(ref) => { this.docsPanel = ref; }} /> */}
+                {(() => {
+                    if (this.props.admin) {
+                        return (
+                            <DocsPanel ref={(ref) => { this.docsPanel = ref; }} />
+                        );
+                    }
+                    return null;
+                })()}
             </div>
         );
     }
 }
+
+Editor.propTypes = {
+    admin: React.PropTypes.bool,
+};
+
+Editor.defaultProps = {
+    admin: false,
+};
+
+function mapStateToProps(state) {
+    return {
+        admin: state.user.admin || false,
+    };
+}
+
+export default connect(mapStateToProps)(Editor);
