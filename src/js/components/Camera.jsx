74b2cbce70978c7f64e02fc60a17d6370a916fea
Lou Huang <lou@louhuang.com>
2016-10-14 12:29:55 -0400

    Add animation states to camera; display pixel size

--- src/js/components/Camera.jsx
+++ src/js/components/Camera.jsx
@@ -35,54 +35,85 @@ class Camera extends React.Component {
   }
 
   render() {
+    let infoText;
+    let classNames = 'camera-component';
+
+    // Add animation classes depending on state
     if (this.props.isVisible) {
-      // Do not display the video button on environments that cannot support it
-      // http://caniuse.com/#search=MediaRecorder
-      // Currently: only Chrome, Firefox, Safari
-      let recordVideoButton;
-      if (typeof window.MediaRecorder === 'function') {
-        let recordIcon = 'bt-circle';
-        let recordTooltip = 'Capture video clip';
+      classNames += ' camera-animate-enter';
+    } else {
+      classNames += ' camera-animate-leave';
+    }
 
-        if (this.state.recording) {
-          recordIcon = 'bt-pause';
-          recordTooltip = 'Stop capturing video and save';
-        }
+    // Add classes when recording
+    if (this.state.recording) {
+      classNames += ' camera-is-recording';
+    }
 
-        recordVideoButton = (
+    // Disable the video button on environments that cannot support it
+    // http://caniuse.com/#search=MediaRecorder
+    // Currently: only Chrome, Firefox, Opera
+    const recordIsDisabled = typeof window.MediaRecorder !== 'function';
+    let recordIcon = 'bt-circle';
+    let recordTooltip = 'Capture video clip';
+
+    if (this.state.recording) {
+      recordIcon = 'bt-square';
+      recordTooltip = 'Stop capturing video and save';
+      infoText = 'Recording…';
+    }
+
+    // Note: tooltips are currently not showing on disabled buttons for
+    // whatever reason.
+    if (recordIsDisabled) {
+      recordTooltip = 'Your browser does not support video capture';
+    }
+
+    // Experimental: show pixel size of the recording.
+    // This makes a hard-coded request to the map element for its size.
+    // Ideally, we should be able to request it from this component's size itself
+    // (it is now set to be 100% of the map) but the problem is we cannot
+    // determine it during render because the component's container element has
+    // not mounted. We can solve this in other ways, but let's prove this is a
+    // good idea first. TODO: respond to changes in browser window and map
+    // element size. TODO: take into account monitor pixel density.
+    if (!this.state.recording) {
+      if (document.getElementById('map')) {
+        const dims = document.getElementById('map').getBoundingClientRect();
+        infoText = `${dims.width}px × ${dims.height}px`;
+      }
+    }
+
+    return (
+      <div className={classNames} ref={ref => { this.el = ref; }}>
+        <div className="camera-controls modal">
+          <IconButton
+            className="camera-screenshot-button"
+            onClick={takeScreenshot}
+            icon="bt-camera"
+            tooltip="Take a screenshot"
+            disabled={this.state.recording === true}
+          />
           <IconButton
             className="camera-record-button"
             onClick={this.onClickRecord}
             icon={recordIcon}
             tooltip={recordTooltip}
+            disabled={recordIsDisabled}
           />
-        );
-      }
-
-      return (
-        <div className="camera-component">
-          <div className="camera-controls modal">
-            <IconButton
-              className="camera-screenshot-button"
-              onClick={takeScreenshot}
-              icon="bt-camera"
-              tooltip="Take a screenshot"
-              disabled={this.state.recording === true}
-            />
-            { recordVideoButton }
-            <IconButton
-              className="camera-close-button"
-              onClick={this.onClickClose}
-              icon="bt-times"
-              tooltip="Close"
-              disabled={this.state.recording === true}
-            />
+          <IconButton
+            className="camera-close-button"
+            onClick={this.onClickClose}
+            icon="bt-times"
+            tooltip="Close"
+            disabled={this.state.recording === true}
+          />
+          <div className="camera-info">
+            {infoText}
           </div>
         </div>
-      );
-    }
-
-    return null;
+      </div>
+    );
   }
 }
 
