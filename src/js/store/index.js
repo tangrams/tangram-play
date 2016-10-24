830f82967d71b6502f3f9e8b33014eea09da2670
Lou Huang <lou@louhuang.com>
2016-10-24 13:37:05 -0400

    Add redux-logger

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,18 +1,22 @@
+/* eslint-disable no-underscore-dangle */
 // Initiate Redux store
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
 // This is where other Redux-related libs are added, e.g.
 // react-router-redux if we use it in the future.
+import createLogger from 'redux-logger';
 
 // Import the root reducer
 import reducers from './reducers';
 
 const initialState = {};
+const logger = createLogger();
 
-/* eslint-disable no-underscore-dangle */
-const store = createStore(reducers, initialState,
-    // For Redux devtools extension.
-    // https://github.com/zalmoxisus/redux-devtools-extension
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// For Redux devtools extension.
+// https://github.com/zalmoxisus/redux-devtools-extension
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, initialState, composeEnhancers(
+  applyMiddleware(logger)
+));
 
 export default store;
