792fa0a569dd55eaf079513583e052c79808c1a5
Lou Huang <lou@louhuang.com>
2016-11-30 11:26:18 -0500

    Remove redux-logger

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,22 +1,18 @@
 /* eslint-disable no-underscore-dangle */
 // Initiate Redux store
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, compose } from 'redux';
 
 // This is where other Redux-related libs are added, e.g.
 // react-router-redux if we use it in the future.
-import createLogger from 'redux-logger';
 
 // Import the root reducer
 import reducers from './reducers';
 
 const initialState = {};
-const logger = createLogger();
 
 // For Redux devtools extension.
 // https://github.com/zalmoxisus/redux-devtools-extension
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, initialState, composeEnhancers(
-  applyMiddleware(logger)
-));
+const store = createStore(reducers, initialState);
 
 export default store;
