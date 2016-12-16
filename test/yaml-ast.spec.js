197b9d1f36b6ebd44b7a069536ed18d9f3832612
Lou Huang <lou@louhuang.com>
2016-12-16 15:36:38 -0500

    Add tests for getNodeAtKeyAddress()

--- test/yaml-ast.spec.js
+++ test/yaml-ast.spec.js
@@ -4,7 +4,7 @@ import { ParsedYAMLDocument, getKeyAddressForNode } from '../src/js/editor/yaml-
 // YAML node kinds are assigned a number by YAMLParser.
 const YAML_SCALAR = 0;
 const YAML_MAPPING = 1;
-const YAML_MAP = 2;
+// const YAML_MAP = 2;
 const YAML_SEQUENCE = 3;
 
 const TEST_DOCUMENT = `
@@ -69,9 +69,36 @@ describe('YAML abstract syntax tree parser', () => {
       const node = parsed.getNodeAtIndex(0);
       assert.isNull(node);
     });
+
+    it('returns null if index is out of bounds', () => {
+      const node = parsed.getNodeAtIndex(9999);
+      assert.isNull(node);
+    });
   });
 
   describe('getNodeAtKeyAddress()', () => {
+    it('returns the mapping node for an address whose value is scalar', () => {
+      const node = parsed.getNodeAtKeyAddress('sources:mapzen:url');
+      assert.equal(node.kind, YAML_MAPPING);
+      assert.equal(node.key.value, 'url');
+    });
+
+    it('returns the mapping node for an address whose value is another map', () => {
+      const node = parsed.getNodeAtKeyAddress('layers:earth');
+      assert.equal(node.kind, YAML_MAPPING);
+      assert.equal(node.key.value, 'earth');
+    });
+
+    it('returns the mapping node for an address whose value is a sequence', () => {
+      const node = parsed.getNodeAtKeyAddress('import');
+      assert.equal(node.kind, YAML_MAPPING);
+      assert.equal(node.key.value, 'import');
+    });
+
+    it('returns null for an address that cannot be found', () => {
+      const node = parsed.getNodeAtKeyAddress('foo:bar');
+      assert.isNull(node);
+    });
   });
 
   describe('getKeyAddressForNode()', () => {
