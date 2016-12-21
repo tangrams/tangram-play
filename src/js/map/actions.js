df7cd62e10c5a5b90d524fe0be948734ec18ed9a
Lou Huang <lou@louhuang.com>
2016-12-21 15:32:51 -0500

    Remove extraneous comments

--- src/js/map/actions.js
+++ src/js/map/actions.js
@@ -4,8 +4,6 @@ import { SET_APP_STATE } from '../store/actions';
 
 /**
  * Shows the scene loading indicator.
- *
- * TODO: Deprecate / remove. Map loading should be set via application state.
  */
 export function showSceneLoadingIndicator() {
   store.dispatch({
@@ -16,8 +14,6 @@ export function showSceneLoadingIndicator() {
 
 /**
  * Hide the scene loading indicator.
- *
- * TODO: Deprecate / remove. Map loading should be set via application state.
  */
 export function hideSceneLoadingIndicator() {
   store.dispatch({
