7c820dd3c5ba0fea6699208d146294990b2bae38
Lou Huang <lou@louhuang.com>
2017-01-25 12:15:27 -0500

    Show what’s new screen if a user returns and the version number has changed.

--- src/js/ui/welcome.js
+++ src/js/ui/welcome.js
@@ -1,16 +1,32 @@
+import VERSION from '../version.json';
+
 // Redux
 import store from '../store';
-import { SHOW_MODAL } from '../store/actions';
+import { SHOW_MODAL, SET_RECENT_VERSION } from '../store/actions';
 
 export function showWelcomeScreen() {
   const persistence = store.getState().persistence;
 
+  if (!persistence) return;
+
   // TEMP: test welcome screen.
-  if (persistence && persistence.welcomeScreenDismissed !== true) {
+  if (persistence.welcomeScreenDismissed !== true) {
     store.dispatch({
       type: SHOW_MODAL,
       modalType: 'WELCOME',
       priority: 100, // Display above error modals
     });
+  } else if (VERSION.v !== persistence.mostRecentVersion) {
+    store.dispatch({
+      type: SHOW_MODAL,
+      modalType: 'WHATS_NEW',
+      priority: 100, // Display above error modals
+    });
   }
+
+  // Store most recent version seen
+  store.dispatch({
+    type: SET_RECENT_VERSION,
+    version: VERSION.v,
+  });
 }
