4cd8306ece95b203462f8f7ac0d1dc54769cc5fc
Brett Camper <bcamper@alum.mit.edu>
2018-01-31 12:07:17 -0500

    commenting unused localforage, still deciding on longer term saving approach

--- src/js/components/MenuBar.jsx
+++ src/js/components/MenuBar.jsx
@@ -1,4 +1,4 @@
-import localforage from 'localforage';
+// import localforage from 'localforage';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
